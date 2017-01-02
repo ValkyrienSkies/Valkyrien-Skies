f0124e29d3ef78f308e3df2ae9c5e97e2968f212
DaMatrix <joey.rabil@gmail.com>
2017-01-02 10:01:08 +0100

    Add base command
    Not sure why I'm making a seperate commit for this but whatever

---
+++ src/main/java/ValkyrienWarfareBase/Command/PhysSettingsCommand.java
@@ -0,0 +1,28 @@
+package ValkyrienWarfareBase.Command;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+
+public class PhysSettingsCommand extends CommandBase {
+
+	@Override
+	public String getCommandName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender sender) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+		// TODO Auto-generated method stub
+
+	}
+
+}

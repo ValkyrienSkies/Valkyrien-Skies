fd0c66c2e3fe926dc835829e86d2b096d484313c
DaMatrix <joey.rabil@gmail.com>
2017-01-12 18:27:12 +0100

    Fix command a bit
    Tab completion and lack of argument protection

--- src/main/java/ValkyrienWarfareBase/Command/AirshipSettingsCommand.java
+++ src/main/java/ValkyrienWarfareBase/Command/AirshipSettingsCommand.java
@@ -1,5 +1,13 @@
 package ValkyrienWarfareBase.Command;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import com.google.common.collect.Lists;
+
 import ValkyrienWarfareBase.ValkyrienWarfareMod;
 import ValkyrienWarfareBase.PhysicsManagement.PhysicsWrapperEntity;
 import net.minecraft.command.CommandBase;
@@ -10,8 +18,16 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 
 public class AirshipSettingsCommand extends CommandBase {
+	
+public static final ArrayList<String> completionOptions = new ArrayList<String>();
+	
+	static {
+		completionOptions.add("transfer");
+		completionOptions.add("allowPlayer");
+	}
 
 	@Override
 	public String getCommandName() {
@@ -30,8 +46,13 @@ public class AirshipSettingsCommand extends CommandBase {
 			return;
 		}
 
+		if (args.length == 0) {
+			sender.addChatMessage(new TextComponentString(TextFormatting.RED + "Usage: " + getCommandUsage(sender)));
+			return;
+		}
+
 		EntityPlayer p = (EntityPlayer) sender;
-		BlockPos pos = p.rayTrace(4.5, 1).getBlockPos();
+		BlockPos pos = p.rayTrace(p.isCreative() ? 5.0 : 4.5, 1).getBlockPos();
 		PhysicsWrapperEntity wrapper = ValkyrienWarfareMod.physicsManager.getObjectManagingPos(p.getEntityWorld(), pos);
 
 		if (wrapper == null) {
@@ -40,7 +61,10 @@ public class AirshipSettingsCommand extends CommandBase {
 		}
 		if (p.entityUniqueID.toString().equals(wrapper.wrapping.creator)) {
 			if (args[0].equals("transfer")) {
-				if (args[1] != null && !args[1].isEmpty()) {
+				if (args.length == 1) {
+					return;
+				}
+				if (!args[1].isEmpty()) {
 					EntityPlayer target = server.getPlayerList().getPlayerByUsername(args[1]);
 					if (target == null) {
 						p.addChatMessage(new TextComponentString("That player is not online!"));
@@ -60,10 +84,23 @@ public class AirshipSettingsCommand extends CommandBase {
 					return;
 				}
 			} else if (args[0].equals("allowPlayer")) {
-				if (args[1] != null && !args[1].isEmpty()) {
+				if (args.length == 1) {
+					StringBuilder result = new StringBuilder("<");
+					Iterator<String> iter = wrapper.wrapping.allowedUsers.iterator();
+					while (iter.hasNext()) {
+						result.append(iter.next() + (iter.hasNext() ? ", " : ">"));
+					}
+					p.addChatMessage(new TextComponentString(result.toString()));
+					return;
+				}
+				if (!args[1].isEmpty()) {
 					EntityPlayer target = server.getPlayerList().getPlayerByUsername(args[1]);
 					if (target == null) {
 						p.addChatMessage(new TextComponentString("That player is not online!"));
+						return;
+					}
+					if (target.entityUniqueID.toString().equals(wrapper.wrapping.creator)) {
+						p.addChatMessage(new TextComponentString("You can't add yourself to your own airship!"));
 						return;
 					}
 					wrapper.wrapping.allowedUsers.add(target.entityUniqueID.toString());
@@ -74,5 +111,32 @@ public class AirshipSettingsCommand extends CommandBase {
 		} else {
 			p.addChatMessage(new TextComponentString("You need to be the owner of an airship to change airship settings!"));
 		}
+	}
+	
+	@Override
+	public List<String> getTabCompletionOptions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos pos) {
+		if (args.length == 1)	{
+			ArrayList<String> possibleArgs = (ArrayList<String>) completionOptions.clone();
+			
+			for (Iterator<String> iterator = possibleArgs.iterator(); iterator.hasNext();) { //Don't like this, but I have to because concurrentmodificationexception			    
+			    if (!iterator.next().startsWith(args[0])) {
+			        iterator.remove();
+			    }
+			}
+			
+			return possibleArgs;
+		} else if (args.length == 2)	{
+			if (args[0].startsWith("do"))	{
+				if (args[1].startsWith("t"))	{
+					return Lists.newArrayList("true");
+				} else if (args[1].startsWith("f"))	{
+					return Lists.newArrayList("false");
+				} else {
+					return Lists.newArrayList("true", "false");
+				}
+			}
+		}
+		
+		return null;
 	}
 }

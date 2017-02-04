2d3e2fe69c93ab85405c60274479d33e1de1bedc
DaMatrix <joey.rabil@gmail.com>
2017-02-04 10:55:01 +0100

    add a constructor i needed

--- src/main/java/ValkyrienWarfareControl/TileEntity/TileEntityNormalEtherCompressor.java
+++ src/main/java/ValkyrienWarfareControl/TileEntity/TileEntityNormalEtherCompressor.java
@@ -4,6 +4,10 @@ import ValkyrienWarfareBase.API.Block.EtherCompressor.TileEntityEtherCompressor;
 
 public class TileEntityNormalEtherCompressor extends TileEntityEtherCompressor {
 	
+	public TileEntityNormalEtherCompressor() {
+		super();
+	}
+	
 	public TileEntityNormalEtherCompressor(double power) {
 		super(power);
 	}

e6fd15748a4dadcc8f62c5efefc51d03446ed53b
DaMatrix <joey.rabil@gmail.com>
2017-02-04 10:16:24 +0100

    base system for new ether compressors

--- src/main/java/ValkyrienWarfareBase/API/Block/EtherCompressor/BlockEtherCompressor.java
+++ src/main/java/ValkyrienWarfareBase/API/Block/EtherCompressor/BlockEtherCompressor.java
@@ -1,10 +1,9 @@
-package ValkyrienWarfareControl.Block;
+package ValkyrienWarfareBase.API.Block.EtherCompressor;
 
 import ValkyrienWarfareBase.API.IBlockForceProvider;
 import ValkyrienWarfareBase.API.Vector;
 import ValkyrienWarfareBase.PhysicsManagement.PhysicsObject;
 import ValkyrienWarfareBase.PhysicsManagement.PhysicsWrapperEntity;
-import ValkyrienWarfareControl.TileEntity.AntiGravEngineTileEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
@@ -14,15 +13,13 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class BlockAntiGravEngine extends Block implements ITileEntityProvider, IBlockForceProvider {
-
-	public BlockAntiGravEngine(Material materialIn) {
+public abstract class BlockEtherCompressor extends Block implements ITileEntityProvider, IBlockForceProvider {
+	
+	public double enginePower = 25000D;
+	
+	public BlockEtherCompressor(Material materialIn, double enginePower) {
 		super(materialIn);
-	}
-
-	@Override
-	public TileEntity createNewTileEntity(World worldIn, int meta) {
-		return new AntiGravEngineTileEntity();
+		this.enginePower = enginePower;
 	}
 
 	@Override
@@ -34,8 +31,8 @@ public class BlockAntiGravEngine extends Block implements ITileEntityProvider, I
 		if (worldTile == null) {
 			return null;
 		}
-		if (worldTile instanceof AntiGravEngineTileEntity) {
-			AntiGravEngineTileEntity engineTile = (AntiGravEngineTileEntity) worldTile;
+		if (worldTile instanceof TileEntityEtherCompressor) {
+			TileEntityEtherCompressor engineTile = (TileEntityEtherCompressor) worldTile;
 			return engineTile.getForceOutput(world, pos, state, wrapper, secondsToApply);
 		}
 		return null;

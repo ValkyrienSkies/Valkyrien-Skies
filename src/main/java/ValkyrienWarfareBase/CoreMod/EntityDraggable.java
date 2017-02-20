c908b41fc1f9af094dcce793ecc6a5df340f2ec9
Alexander Mastrangelo <u0828016@utah.edu>
2017-02-20 02:34:10 -0700

    Entity Superclass Overriden!

---
+++ src/main/java/ValkyrienWarfareBase/CoreMod/EntityDraggable.java
@@ -0,0 +1,10 @@
+package ValkyrienWarfareBase.CoreMod;
+
+public class EntityDraggable {
+
+	public int testNumber = -5;
+	
+	public EntityDraggable(){
+		
+	}
+}
--- src/main/java/ValkyrienWarfareBase/CoreMod/ValkyrienWarfareTransformer.java
+++ src/main/java/ValkyrienWarfareBase/CoreMod/ValkyrienWarfareTransformer.java
@@ -1,11 +1,16 @@
 package ValkyrienWarfareBase.CoreMod;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.util.CheckClassAdapter;
 
 import net.minecraft.launchwrapper.IClassTransformer;
@@ -22,13 +27,17 @@ public class ValkyrienWarfareTransformer implements IClassTransformer {
 					return classData;
 				}
 			}
-			TransformAdapter adapter = new TransformAdapter(Opcodes.ASM4, ValkyrienWarfarePlugin.isObfuscatedEnvironment);
+			
+
+			TransformAdapter adapter = new TransformAdapter(Opcodes.ASM4, ValkyrienWarfarePlugin.isObfuscatedEnvironment, transformedName);
 			ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
 			adapter.setCV(classWriter);
 			try {
 				new ClassReader(classData).accept(adapter, ClassReader.EXPAND_FRAMES);
-				// Unsafe transformed bytes, JVM needs to reformat them
 				byte[] byteArray = classWriter.toByteArray();
+				//Extra transforms that don't fall into typical scenarios go here; like changing the SuperClass of Entity
+				byteArray = ASMUtils.doAdditionalTransforms(transformedName, byteArray);
+				//Performs sanity checks and frame stack recalculations before pushing the new bytecode
 				ClassReader cr = new ClassReader(byteArray);
 				ClassWriter checkedWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
 				CheckClassAdapter adapterChecker = new CheckClassAdapter(checkedWriter, true);

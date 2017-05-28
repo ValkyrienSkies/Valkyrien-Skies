6f99f71a45644e4bb5dbc6b6e1a40c74422f1cce
Alexander Mastrangelo <u0828016@utah.edu>
2017-05-27 23:46:19 -0600

    Fixed ALL the model errors

--- src/main/java/com/jackredcreeper/cannon/CannonModRefrence.java
+++ src/main/java/com/jackredcreeper/cannon/CannonModRefrence.java
@@ -5,57 +5,57 @@ public class CannonModRefrence {
 	public static final String MOD_ID = "cannons";
 	public static final String NAME = "Cannons!";
 	public static final String VERSION = "1";
-			
+
 	public static final String CLIENT = "com.jackredcreeper.cannon.proxy.ClientProxy";
 	public static final String SERVER = "com.jackredcreeper.cannon.proxy.ServerProxy";
-	
+
 	public static enum ModItems {
-		PRIMER("PRIMER","ItemPrimer"),
-		LOADER("LOADER","ItemLoader"),
-		TUNER("TUNER","ItemTuner"),
-		CANNONBALL("CANNONBALL","ItemCannonball"),
-		EXPLOSIVEBALL("EXPLOSIVEBALL","ItemExplosiveball"),
-		GRAPESHOT("GRAPESHOT","ItemGrapeshot"),
-		SOLIDBALL("SOLIDBALL","ItemSolidball");
-		
+		PRIMER("PRIMER","itemprimer"),
+		LOADER("LOADER","itemloader"),
+		TUNER("TUNER","itemtuner"),
+		CANNONBALL("CANNONBALL","itemcannonball"),
+		EXPLOSIVEBALL("EXPLOSIVEBALL","itemexplosiveball"),
+		GRAPESHOT("GRAPESHOT","itemgrapeshot"),
+		SOLIDBALL("SOLIDBALL","itemsolidball");
+
 		private String unlocalizedName;
 		private String registryName;
-		
+
 		ModItems(String unlocalizedName, String registryName) {
 			this.unlocalizedName = unlocalizedName;
 			this.registryName = registryName;
-			
+
 		}
-		
+
 		public String getUnlocalizedName() {
 			return unlocalizedName;
 		}
-		
+
 		public String getRegistryName() {
 			return registryName;
 		}
-	}	
-	
+	}
+
 	public static enum ModBlocks {
-		CANNON("CANNON","BlockCannon");
-		
+		CANNON("CANNON","blockcannon");
+
 		private String unlocalizedName;
 		private String registryName;
-		
+
 		ModBlocks(String unlocalizedName, String registryName) {
 			this.unlocalizedName = unlocalizedName;
 			this.registryName = registryName;
-			
+
 		}
-		
+
 		public String getUnlocalizedName() {
 			return unlocalizedName;
 		}
-		
+
 		public String getRegistryName() {
 			return registryName;
-		}	
+		}
 	}
-	
-	
+
+
 }

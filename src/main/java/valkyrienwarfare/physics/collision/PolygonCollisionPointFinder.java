ccf108ebc4dd27ee495d972474a06ffa85bb36d2
Alexander Mastrangelo <best108@uw.edu>
2018-02-04 06:03:22 -0800

    Misc code reformating

--- src/main/java/valkyrienwarfare/physics/collision/PolygonCollisionPointFinder.java
+++ src/main/java/valkyrienwarfare/physics/collision/PolygonCollisionPointFinder.java
@@ -4,61 +4,48 @@ import valkyrienwarfare.api.Vector;
 
 public class PolygonCollisionPointFinder {
 
-	//TODO: This algorithm isn't correct, fix it later on!
-	public static Vector[] getPointsOfCollisionForPolygons(PhysPolygonCollider collider, PhysCollisionObject somethingElse, Vector velocity){
+    // TODO: This algorithm isn't correct, fix it later on!
+    public static Vector[] getPointsOfCollisionForPolygons(PhysPolygonCollider collider,
+            PhysCollisionObject somethingElse, Vector velocity) {
+        double minSecondsAgo = 69D;
+        int minCollisionIndex = 0;
+        int minCollisionIndexIndex = 0;
 
-//		Vector axis = new Vector(toCollideWith.axis);
+        for (int cont = 0; cont < collider.collisions.length; cont++) {
+            PhysCollisionObject toCollideWith = collider.collisions[cont];
+            Vector axis = somethingElse.axis;
+            double reverseVelocityAlongAxis = -velocity.dot(axis);
+            double minDot = 9999999999999D;
+            int minIndex = 0;
 
-		double minSecondsAgo = 69D;
-		int minCollisionIndex = 0;
-		int minCollisionIndexIndex = 0;
+            for (int i = 0; i < 8; i++) {
+                Vector vertice = toCollideWith.movable.getVertices()[i];
+                double dot = vertice.dot(axis) * reverseVelocityAlongAxis;
+                if (dot < minDot) {
+                    minDot = dot;
+                    minIndex = i;
+                }
+            }
 
-		for(int cont = 0; cont < collider.collisions.length; cont++){
-			PhysCollisionObject toCollideWith = collider.collisions[cont];
+            Vector contactPoint = toCollideWith.movable.getVertices()[minIndex];
+            double secondsAgo = 69;
+            if (Math.signum(reverseVelocityAlongAxis) == 1.0D) {
+                secondsAgo = toCollideWith.movMinFixMax / reverseVelocityAlongAxis;
+            } else {
+                secondsAgo = toCollideWith.movMaxFixMin / reverseVelocityAlongAxis;
+            }
 
-			Vector axis = somethingElse.axis;
-			double reverseVelocityAlongAxis = -velocity.dot(axis);
-			double minDot = 9999999999999D;
-			int minIndex = 0;
+            if (secondsAgo < minSecondsAgo) {
+                minSecondsAgo = secondsAgo;
+                minCollisionIndex = cont;
+                minCollisionIndexIndex = minIndex;
+            }
+        }
 
-			for(int i = 0; i < 8; i++){
-				Vector vertice = toCollideWith.movable.getVertices()[i];
-				double dot = vertice.dot(axis) * reverseVelocityAlongAxis;
-				if(dot < minDot){
-					minDot = dot;
-					minIndex = i;
-				}
-			}
-
-			Vector contactPoint = toCollideWith.movable.getVertices()[minIndex];
-
-			double secondsAgo = 69;
-
-			if(Math.signum(reverseVelocityAlongAxis) == 1.0D){
-				secondsAgo = toCollideWith.movMinFixMax / reverseVelocityAlongAxis;
-			}else{
-				secondsAgo = toCollideWith.movMaxFixMin / reverseVelocityAlongAxis;
-			}
-
-			if(secondsAgo < minSecondsAgo){
-				minSecondsAgo = secondsAgo;
-				minCollisionIndex = cont;
-				minCollisionIndexIndex = minIndex;
-			}
-		}
-
-
-
-//		System.out.println(contactPoint);
-
-		Vector contactPoint = collider.collisions[minCollisionIndex].movable.getVertices()[minCollisionIndexIndex];//contactPoint.getAddition(axis.getProduct(reverseVelocityAlongAxis * secondsAgo));
-
-		return new Vector[]{
-//				contactPoint
-				collider.collisions[minCollisionIndex].firstContactPoint,
-				collider.collisions[minCollisionIndex].getSecondContactPoint()
-		};
-	}
-
+        return new Vector[] {
+                // contactPoint
+                collider.collisions[minCollisionIndex].firstContactPoint,
+                collider.collisions[minCollisionIndex].getSecondContactPoint() };
+    }
 
 }

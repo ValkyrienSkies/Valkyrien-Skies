2d9ff4c2d08cb9e3b56767a0a6425170259aa419
Alexander Mastrangelo <best108@uw.edu>
2018-02-03 22:16:22 -0800

    Pork Fix: v1

--- src/main/java/valkyrienwarfare/mixin/network/play/client/MixinCPacketPlayerDigging.java
+++ src/main/java/valkyrienwarfare/mixin/network/play/client/MixinCPacketPlayerDigging.java
@@ -16,14 +16,16 @@
 
 package valkyrienwarfare.mixin.network.play.client;
 
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.network.NetHandlerPlayServer;
-import net.minecraft.network.play.INetHandlerPlayServer;
-import net.minecraft.network.play.client.CPacketPlayerDigging;
-import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.network.PacketThreadUtil;
+import net.minecraft.network.play.INetHandlerPlayServer;
+import net.minecraft.network.play.client.CPacketPlayerDigging;
+import net.minecraft.util.math.BlockPos;
 import valkyrienwarfare.ValkyrienWarfareMod;
 import valkyrienwarfare.api.RotationMatrices;
 import valkyrienwarfare.mod.physmanagement.interaction.INHPServerVW;
@@ -32,30 +34,29 @@ import valkyrienwarfare.physics.management.PhysicsWrapperEntity;
 
 @Mixin(CPacketPlayerDigging.class)
 public abstract class MixinCPacketPlayerDigging {
-    @Redirect(method = "processPacket",
-            at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/network/play/INetHandlerPlayServer;processPlayerDigging(Lnet/minecraft/network/play/client/CPacketPlayerDigging;)V"))
-    public void handleDiggingPacket(INetHandlerPlayServer server, CPacketPlayerDigging packetIn) {
-        EntityPlayerMP player = ((NetHandlerPlayServer) server).player;
-        INHPServerVW vw = (INHPServerVW) (NetHandlerPlayServer) server;
+	@Redirect(method = "processPacket", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/play/INetHandlerPlayServer;processPlayerDigging(Lnet/minecraft/network/play/client/CPacketPlayerDigging;)V"))
+	public void handleDiggingPacket(INetHandlerPlayServer server, CPacketPlayerDigging packetIn) {
+		EntityPlayerMP player = ((NetHandlerPlayServer) server).player;
+		PacketThreadUtil.checkThreadAndEnqueue(packetIn, server, player.getServerWorld());
+		INHPServerVW vw = (INHPServerVW) (NetHandlerPlayServer) server;
 
-        BlockPos packetPos = packetIn.getPosition();
-        PlayerDataBackup playerBackup = new PlayerDataBackup(player);
-        PhysicsWrapperEntity wrapper = ValkyrienWarfareMod.physicsManager.getObjectManagingPos(player.world, packetPos);
-        if (player.interactionManager.getBlockReachDistance() != vw.dummyBlockReachDist()) {
-            vw.lastGoodBlockReachDist(player.interactionManager.getBlockReachDistance());
-        }
-        if (wrapper != null) {
-            player.interactionManager.setBlockReachDistance(vw.dummyBlockReachDist());
-        }
-        if (wrapper != null && wrapper.wrapping.coordTransform != null) {
-            RotationMatrices.applyTransform(wrapper.wrapping.coordTransform.wToLTransform, wrapper.wrapping.coordTransform.wToLRotation, player);
-            server.processPlayerDigging(packetIn);
-            RotationMatrices.applyTransform(wrapper.wrapping.coordTransform.lToWTransform, wrapper.wrapping.coordTransform.lToWRotation, player);
-            playerBackup.restorePlayerToBackup();
-        } else {
-            server.processPlayerDigging(packetIn);
-        }
-        player.interactionManager.setBlockReachDistance(vw.lastGoodBlockReachDist());
-    }
+		BlockPos packetPos = packetIn.getPosition();
+		PlayerDataBackup playerBackup = new PlayerDataBackup(player);
+		PhysicsWrapperEntity wrapper = ValkyrienWarfareMod.physicsManager.getObjectManagingPos(player.world, packetPos);
+		if (player.interactionManager.getBlockReachDistance() != vw.dummyBlockReachDist()) {
+			vw.lastGoodBlockReachDist(player.interactionManager.getBlockReachDistance());
+		}
+		if (wrapper != null) {
+			player.interactionManager.setBlockReachDistance(vw.dummyBlockReachDist());
+		}
+		if (wrapper != null && wrapper.wrapping.coordTransform != null) {
+			RotationMatrices.applyTransform(wrapper.wrapping.coordTransform.wToLTransform, wrapper.wrapping.coordTransform.wToLRotation, player);
+			server.processPlayerDigging(packetIn);
+			RotationMatrices.applyTransform(wrapper.wrapping.coordTransform.lToWTransform, wrapper.wrapping.coordTransform.lToWRotation, player);
+			playerBackup.restorePlayerToBackup();
+		} else {
+			server.processPlayerDigging(packetIn);
+		}
+		player.interactionManager.setBlockReachDistance(vw.lastGoodBlockReachDist());
+	}
 }
--- src/main/java/valkyrienwarfare/mixin/network/play/client/MixinCPacketPlayerTryUseItemOnBlock.java
+++ src/main/java/valkyrienwarfare/mixin/network/play/client/MixinCPacketPlayerTryUseItemOnBlock.java
@@ -16,15 +16,17 @@
 
 package valkyrienwarfare.mixin.network.play.client;
 
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.item.ItemBucket;
-import net.minecraft.network.NetHandlerPlayServer;
-import net.minecraft.network.play.INetHandlerPlayServer;
-import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
-import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemBucket;
+import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.network.PacketThreadUtil;
+import net.minecraft.network.play.INetHandlerPlayServer;
+import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
+import net.minecraft.util.math.BlockPos;
 import valkyrienwarfare.ValkyrienWarfareMod;
 import valkyrienwarfare.api.RotationMatrices;
 import valkyrienwarfare.mod.physmanagement.interaction.INHPServerVW;
@@ -33,36 +35,35 @@ import valkyrienwarfare.physics.management.PhysicsWrapperEntity;
 
 @Mixin(CPacketPlayerTryUseItemOnBlock.class)
 public abstract class MixinCPacketPlayerTryUseItemOnBlock {
-    @Redirect(method = "processPacket",
-            at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/network/play/INetHandlerPlayServer;processTryUseItemOnBlock(Lnet/minecraft/network/play/client/CPacketPlayerTryUseItemOnBlock;)V"))
-    public void handleUseItemPacket(INetHandlerPlayServer server, CPacketPlayerTryUseItemOnBlock packetIn) {
-        EntityPlayerMP player = ((NetHandlerPlayServer) server).player;
-        INHPServerVW vw = (INHPServerVW) (NetHandlerPlayServer) server;
+	@Redirect(method = "processPacket", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/play/INetHandlerPlayServer;processTryUseItemOnBlock(Lnet/minecraft/network/play/client/CPacketPlayerTryUseItemOnBlock;)V"))
+	public void handleUseItemPacket(INetHandlerPlayServer server, CPacketPlayerTryUseItemOnBlock packetIn) {
+		EntityPlayerMP player = ((NetHandlerPlayServer) server).player;
+		PacketThreadUtil.checkThreadAndEnqueue(packetIn, server, player.getServerWorld());
+		INHPServerVW vw = (INHPServerVW) (NetHandlerPlayServer) server;
 
-        BlockPos packetPos = packetIn.getPos();
-        PlayerDataBackup playerBackup = new PlayerDataBackup(player);
-        PhysicsWrapperEntity wrapper = ValkyrienWarfareMod.physicsManager.getObjectManagingPos(player.world, packetPos);
-        if (player.interactionManager.getBlockReachDistance() != vw.dummyBlockReachDist()) {
-            vw.lastGoodBlockReachDist(player.interactionManager.getBlockReachDistance());
-        }
-        if (wrapper != null) {
-            player.interactionManager.setBlockReachDistance(vw.dummyBlockReachDist());
-        }
-        if (wrapper != null && wrapper.wrapping.coordTransform != null) {
-            RotationMatrices.applyTransform(wrapper.wrapping.coordTransform.wToLTransform, wrapper.wrapping.coordTransform.wToLRotation, player);
-            if (player.getHeldItem(packetIn.getHand()) != null && player.getHeldItem(packetIn.getHand()).getItem() instanceof ItemBucket) {
-                player.interactionManager.setBlockReachDistance(vw.lastGoodBlockReachDist());
-            }
-            try {
-                server.processTryUseItemOnBlock(packetIn);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            playerBackup.restorePlayerToBackup();
-        } else {
-            server.processTryUseItemOnBlock(packetIn);
-        }
-        player.interactionManager.setBlockReachDistance(vw.lastGoodBlockReachDist());
-    }
+		BlockPos packetPos = packetIn.getPos();
+		PlayerDataBackup playerBackup = new PlayerDataBackup(player);
+		PhysicsWrapperEntity wrapper = ValkyrienWarfareMod.physicsManager.getObjectManagingPos(player.world, packetPos);
+		if (player.interactionManager.getBlockReachDistance() != vw.dummyBlockReachDist()) {
+			vw.lastGoodBlockReachDist(player.interactionManager.getBlockReachDistance());
+		}
+		if (wrapper != null) {
+			player.interactionManager.setBlockReachDistance(vw.dummyBlockReachDist());
+		}
+		if (wrapper != null && wrapper.wrapping.coordTransform != null) {
+			RotationMatrices.applyTransform(wrapper.wrapping.coordTransform.wToLTransform, wrapper.wrapping.coordTransform.wToLRotation, player);
+			if (player.getHeldItem(packetIn.getHand()) != null && player.getHeldItem(packetIn.getHand()).getItem() instanceof ItemBucket) {
+				player.interactionManager.setBlockReachDistance(vw.lastGoodBlockReachDist());
+			}
+			try {
+				server.processTryUseItemOnBlock(packetIn);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			playerBackup.restorePlayerToBackup();
+		} else {
+			server.processTryUseItemOnBlock(packetIn);
+		}
+		player.interactionManager.setBlockReachDistance(vw.lastGoodBlockReachDist());
+	}
 }

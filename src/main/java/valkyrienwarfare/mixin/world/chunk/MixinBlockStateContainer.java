a91f4d07e587879acdac972121d65c898dd3e92d
Alexander Mastrangelo <best108@uw.edu>
2018-02-04 23:32:31 -0800

    Fixed issue #124
    With a lengthy message left behind explaining my thoughts.

--- src/main/java/valkyrienwarfare/mixin/world/chunk/MixinBlockStateContainer.java
+++ src/main/java/valkyrienwarfare/mixin/world/chunk/MixinBlockStateContainer.java
@@ -3,9 +3,6 @@ package valkyrienwarfare.mixin.world.chunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BitArray;
@@ -18,34 +15,34 @@ import valkyrienwarfare.physics.collision.optimization.SimpleBitOctree;
 @Mixin(BlockStateContainer.class)
 public class MixinBlockStateContainer implements IBitOctreeProvider {
 
-	private static final int BOTTOM_FOUR_BITMASK = 0xF;
-	private static final int MIDDLE_FOUR_BITMASK = 0xF0;
-	private static final int TOP_FOUR_BITMASK = 0xF00;
-	private IBitOctree bitOctree;
-	@Shadow
-	IBlockStatePalette palette;
-	@Shadow
-	BitArray storage;
+    private final IBitOctree bitOctree = new SimpleBitOctree();
+    @Shadow
+    IBlockStatePalette palette;
+    @Shadow
+    BitArray storage;
 
-	@Inject(method = "<init>()V", at = @At("RETURN"))
-	private void onInit(CallbackInfo ci) {
-		bitOctree = new SimpleBitOctree();
-	}
+    // I know overwrite is a bit overkill here, but I am avoiding using
+    // redirects to prevent CallBack objects from being created. This set() 
+    // method is very low level, and creating new objects within it will cause
+    // serious performance problems.
+    // Also this method is set to protected in the BlockStateContainer class, 
+    // but its safer to set it to public to prevent conflicts with access 
+    // transformers from other mods.
+    // ~Alex Mastrangelo
+    @Overwrite
+    public void set(int index, IBlockState state) {
+        int i = this.palette.idFor(state);
+        this.storage.setAt(index, i);
+        // VW code starts here:
+        int x = index & 0xF;
+        int z = (index & 0xF0) >> 4;
+        int y = (index & 0xF00) >> 8;
+        boolean isStateSolid = state.getMaterial().isSolid();
+        bitOctree.set(x & 15, y & 15, z & 15, isStateSolid);
+    }
 
-	@Overwrite
-	protected void set(int index, IBlockState state) {
-		int i = this.palette.idFor(state);
-		this.storage.setAt(index, i);
-		// VW code starts here:
-		int x = index & BOTTOM_FOUR_BITMASK;
-		int z = (index & MIDDLE_FOUR_BITMASK) >> 4;
-		int y = (index & TOP_FOUR_BITMASK) >> 8;
-		boolean isStateSolid = state.getMaterial().isSolid();
-		bitOctree.set(x & 15, y & 15, z & 15, isStateSolid);
-	}
-
-	@Override
-	public IBitOctree getBitOctree() {
-		return bitOctree;
-	}
+    @Override
+    public IBitOctree getBitOctree() {
+        return bitOctree;
+    }
 }

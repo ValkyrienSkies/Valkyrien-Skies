7f681b9bb7e85b5e9efa5e04e1ef827ddb44b6f5
DaMatrix <joey.rabil@gmail.com>
2018-05-01 17:48:32 +0200

    don't render ether compressors as full blocks

--- src/main/java/valkyrienwarfare/api/block/ethercompressor/BlockEtherCompressor.java
+++ src/main/java/valkyrienwarfare/api/block/ethercompressor/BlockEtherCompressor.java
@@ -22,7 +22,9 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import valkyrienwarfare.api.IBlockForceProvider;
 import valkyrienwarfare.api.Vector;
@@ -60,5 +62,15 @@ public abstract class BlockEtherCompressor extends Block implements ITileEntityP
 
     public void setEnginePower(double power) {
         this.enginePower = power;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+        return true;
     }
 }

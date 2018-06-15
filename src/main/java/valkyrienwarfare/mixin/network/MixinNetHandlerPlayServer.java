cd117f59f7d86e01bb241d4d213f552293dee763
Alexander Mastrangelo <best108@uw.edu>
2018-06-15 03:49:11 -0700

    Survival players no longer kicked for invalid move

--- src/main/java/valkyrienwarfare/mixin/network/MixinNetHandlerPlayServer.java
+++ src/main/java/valkyrienwarfare/mixin/network/MixinNetHandlerPlayServer.java
@@ -21,14 +21,21 @@ import java.util.Set;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.google.common.primitives.Doubles;
+import com.google.common.primitives.Floats;
+
+import net.minecraft.entity.MoverType;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.MobEffects;
 import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.network.PacketThreadUtil;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.network.play.server.SPacketPlayerPosLook;
 import net.minecraft.server.MinecraftServer;
@@ -37,6 +44,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.GameType;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import valkyrienwarfare.ValkyrienWarfareMod;
@@ -95,6 +104,246 @@ public abstract class MixinNetHandlerPlayServer {
 
 	private boolean redirectingSetPlayerLocation = false;
 
+	private static boolean isVectorInvalid(VectorImmutable vector) {
+		if (Doubles.isFinite(vector.getX()) && Doubles.isFinite(vector.getY()) && Doubles.isFinite(vector.getZ())) {
+			return Math.abs(vector.getX()) > 3.0E7D || Math.abs(vector.getY()) > 3.0E7D
+					|| Math.abs(vector.getZ()) > 3.0E7D;
+		} else {
+			return true;
+		}
+	}
+
+	@Inject(method = "processPlayer", at = @At("HEAD"))
+	private void preProcessPlayer(CPacketPlayer packetIn, CallbackInfo info) {
+		WorldServer worldserver = this.serverController.getWorld(this.player.dimension);
+		if (worldserver.isCallingFromMinecraftThread()) {
+			PhysicsWrapperEntity worldBelow = IDraggable.class.cast(this.player).getForcedSubspaceBelowFeet();
+			if (worldBelow != null) {
+				ISubspacedEntityRecord record = worldBelow.getPhysicsObject().getSubspace()
+						.getRecordForSubspacedEntity(ISubspacedEntity.class.cast(this.player));
+				if (record != null) {
+					VectorImmutable position = record.getPositionInGlobalCoordinates();
+					VectorImmutable lookDirection = record.getPositionInGlobalCoordinates();
+					VectorImmutable velocity = record.getVelocityInGlobalCoordinates();
+
+					Vector positionMutable = position.createMutibleVectorCopy();
+					Vector playerPosition = new Vector(player);
+					positionMutable.subtract(playerPosition);
+					if (positionMutable.lengthSq() > 10000 || isVectorInvalid(position)
+							|| isVectorInvalid(lookDirection) || isVectorInvalid(velocity)) {
+						// Bad packet, ignore it.
+					} else {
+						// ===== HACKY STUFF STARTS HERE =====
+						this.firstGoodX = this.lastGoodX = position.getX();
+						this.firstGoodY = this.lastGoodY = position.getY();
+						this.firstGoodZ = this.lastGoodZ = position.getZ();
+
+						float pitch = (float) VWMath.getPitchFromVectorImmutable(lookDirection);
+						float yaw = (float) VWMath.getYawFromVectorImmutable(lookDirection, pitch);
+
+						// this.player.setPositionAndRotation(position.getX(), position.getY(),
+						// position.getZ(), yaw,
+						// pitch);
+
+						this.player.motionX = position.getX() - player.posX;
+						this.player.motionY = position.getY() - player.posY;
+						this.player.motionZ = position.getZ() - player.posZ;
+
+						// ===== NOW FIX THE PACKET =====
+						// CPacketPlayer fixed = new CPacketPlayer();
+						player.posX = packetIn.x = position.getX();
+						player.posY = packetIn.y = position.getY();
+						player.posZ = packetIn.z = position.getZ();
+						packetIn.moving = true;
+						packetIn.onGround = true;
+						// player.rotationYaw = yaw;
+						// player.rotationPitch = pitch;
+						// Dangerous code here
+						// packetIn.rotating = false;
+						// packetIn.pitch = pitch;
+						// packetIn.yaw = yaw;
+
+						// player.setPositionAndRotation(position.getX(), position.getY(),
+						// position.getZ(), yaw, pitch);
+
+						// System.out.println("Used one");
+
+						// packetIn.onGround = true;
+						// packetIn.pitch = pitch;
+						// packetIn.yaw = yaw;
+					}
+
+					worldBelow.getPhysicsObject().getSubspace()
+							.forceSubspaceRecord(ISubspacedEntity.class.cast(this.player), null);
+				}
+			}
+		}
+	}
+
+	@Shadow
+	private static boolean isMovePlayerPacketInvalid(CPacketPlayer packetIn) {
+		return false;
+	}
+
+	/*
+	 * @Overwrite public void processPlayer(CPacketPlayer packetIn) {
+	 * PacketThreadUtil.checkThreadAndEnqueue(packetIn, thisAsNetHandler,
+	 * this.player.getServerWorld());
+	 * 
+	 * if (isMovePlayerPacketInvalid(packetIn)) { this.disconnect(new
+	 * TextComponentTranslation("multiplayer.disconnect.invalid_player_movement",
+	 * new Object[0])); } else { WorldServer worldserver =
+	 * this.serverController.getWorld(this.player.dimension);
+	 * 
+	 * if (!this.player.queuedEndExit) { if (this.networkTickCount == 0) {
+	 * this.captureCurrentPosition(); }
+	 * 
+	 * if (this.targetPos != null) { if (this.networkTickCount -
+	 * this.lastPositionUpdate > 20) { this.lastPositionUpdate =
+	 * this.networkTickCount; this.setPlayerLocation(this.targetPos.x,
+	 * this.targetPos.y, this.targetPos.z, this.player.rotationYaw,
+	 * this.player.rotationPitch); } } else { this.lastPositionUpdate =
+	 * this.networkTickCount;
+	 * 
+	 * if (this.player.isRiding()) {
+	 * this.player.setPositionAndRotation(this.player.posX, this.player.posY,
+	 * this.player.posZ, packetIn.getYaw(this.player.rotationYaw),
+	 * packetIn.getPitch(this.player.rotationPitch));
+	 * this.serverController.getPlayerList().serverUpdateMovingPlayer(this.player);
+	 * } else {
+	 * 
+	 * PhysicsWrapperEntity worldBelow =
+	 * IDraggable.class.cast(this.player).getForcedSubspaceBelowFeet(); if
+	 * (worldBelow != null) { ISubspacedEntityRecord record =
+	 * worldBelow.getPhysicsObject().getSubspace()
+	 * .getRecordForSubspacedEntity(ISubspacedEntity.class.cast(this.player)); if
+	 * (record != null) { VectorImmutable position =
+	 * record.getPositionInGlobalCoordinates(); VectorImmutable lookDirection =
+	 * record.getPositionInGlobalCoordinates(); VectorImmutable velocity =
+	 * record.getVelocityInGlobalCoordinates();
+	 * 
+	 * Vector positionMutable = position.createMutibleVectorCopy(); Vector
+	 * playerPosition = new Vector(player);
+	 * positionMutable.subtract(playerPosition); if (positionMutable.lengthSq() >
+	 * 1000 || isVectorInvalid(position) || isVectorInvalid(lookDirection) ||
+	 * isVectorInvalid(velocity)) { // Bad packet, ignore it. } else { // =====
+	 * HACKY STUFF STARTS HERE ===== this.firstGoodX = this.lastGoodX =
+	 * position.getX(); this.firstGoodY = this.lastGoodY = position.getY();
+	 * this.firstGoodZ = this.lastGoodZ = position.getZ();
+	 * 
+	 * float pitch = (float) VWMath.getPitchFromVectorImmutable(lookDirection);
+	 * float yaw = (float) VWMath.getYawFromVectorImmutable(lookDirection, pitch);
+	 * 
+	 * // this.player.setPositionAndRotation(position.getX(), position.getY(), //
+	 * position.getZ(), yaw, // pitch);
+	 * 
+	 * this.player.motionX = position.getX() - player.posX; this.player.motionY =
+	 * position.getY() - player.posY; this.player.motionZ = position.getZ() -
+	 * player.posZ;
+	 * 
+	 * // ===== NOW FIX THE PACKET ===== // CPacketPlayer fixed = new
+	 * CPacketPlayer(); packetIn.x = position.getX(); packetIn.y = position.getY();
+	 * packetIn.z = position.getZ(); packetIn.moving = true; packetIn.onGround =
+	 * true; // packetIn.rotating = true; // packetIn.pitch = pitch; // packetIn.yaw
+	 * = yaw;
+	 * 
+	 * // player.setPositionAndRotation(position.getX(), position.getY(), //
+	 * position.getZ(), yaw, pitch);
+	 * 
+	 * System.out.println("Used one");
+	 * 
+	 * // packetIn.onGround = true; // packetIn.pitch = pitch; // packetIn.yaw =
+	 * yaw; }
+	 * 
+	 * worldBelow.getPhysicsObject().getSubspace()
+	 * .forceSubspaceRecord(ISubspacedEntity.class.cast(this.player), null); } }
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * double d0 = this.player.posX; double d1 = this.player.posY; double d2 =
+	 * this.player.posZ; double d3 = this.player.posY; double d4 =
+	 * packetIn.getX(this.player.posX); double d5 = packetIn.getY(this.player.posY);
+	 * double d6 = packetIn.getZ(this.player.posZ); float f =
+	 * packetIn.getYaw(this.player.rotationYaw); float f1 =
+	 * packetIn.getPitch(this.player.rotationPitch); double d7 = d4 -
+	 * this.firstGoodX; double d8 = d5 - this.firstGoodY; double d9 = d6 -
+	 * this.firstGoodZ; double d10 = this.player.motionX * this.player.motionX +
+	 * this.player.motionY * this.player.motionY + this.player.motionZ *
+	 * this.player.motionZ; double d11 = d7 * d7 + d8 * d8 + d9 * d9;
+	 * 
+	 * if (this.player.isPlayerSleeping()) { if (d11 > 1.0D) {
+	 * this.setPlayerLocation(this.player.posX, this.player.posY, this.player.posZ,
+	 * packetIn.getYaw(this.player.rotationYaw),
+	 * packetIn.getPitch(this.player.rotationPitch)); } } else {
+	 * ++this.movePacketCounter; int i = this.movePacketCounter -
+	 * this.lastMovePacketCounter;
+	 * 
+	 * if (i > 5) { LOGGER.
+	 * debug("{} is sending move packets too frequently ({} packets since last tick)"
+	 * , this.player.getName(), Integer.valueOf(i)); i = 1; }
+	 * 
+	 * if (!this.player.isInvulnerableDimensionChange() &&
+	 * (!this.player.getServerWorld().getGameRules().getBoolean(
+	 * "disableElytraMovementCheck") || !this.player.isElytraFlying())) { float f2 =
+	 * this.player.isElytraFlying() ? 300.0F : 100.0F;
+	 * 
+	 * if (false && d11 - d10 > (double)(f2 * (float)i) &&
+	 * (!this.serverController.isSinglePlayer() ||
+	 * !this.serverController.getServerOwner().equals(this.player.getName()))) {
+	 * LOGGER.warn("{} moved too quickly! {},{},{}", this.player.getName(),
+	 * Double.valueOf(d7), Double.valueOf(d8), Double.valueOf(d9));
+	 * this.setPlayerLocation(this.player.posX, this.player.posY, this.player.posZ,
+	 * this.player.rotationYaw, this.player.rotationPitch); return; } }
+	 * 
+	 * boolean flag2 = worldserver.getCollisionBoxes(this.player,
+	 * this.player.getEntityBoundingBox().shrink(0.0625D)).isEmpty(); d7 = d4 -
+	 * this.lastGoodX; d8 = d5 - this.lastGoodY; d9 = d6 - this.lastGoodZ;
+	 * 
+	 * if (this.player.onGround && !packetIn.isOnGround() && d8 > 0.0D) {
+	 * this.player.jump(); }
+	 * 
+	 * this.player.move(MoverType.PLAYER, d7, d8, d9); this.player.onGround =
+	 * packetIn.isOnGround(); double d12 = d8; d7 = d4 - this.player.posX; d8 = d5 -
+	 * this.player.posY;
+	 * 
+	 * if (d8 > -0.5D || d8 < 0.5D) { d8 = 0.0D; }
+	 * 
+	 * d9 = d6 - this.player.posZ; d11 = d7 * d7 + d8 * d8 + d9 * d9; boolean flag =
+	 * false;
+	 * 
+	 * if (!this.player.isInvulnerableDimensionChange() && d11 > 0.0625D &&
+	 * !this.player.isPlayerSleeping() &&
+	 * !this.player.interactionManager.isCreative() &&
+	 * this.player.interactionManager.getGameType() != GameType.SPECTATOR) { flag =
+	 * true; LOGGER.warn("{} moved wrongly!", (Object)this.player.getName()); }
+	 * 
+	 * this.player.setPositionAndRotation(d4, d5, d6, f, f1);
+	 * this.player.addMovementStat(this.player.posX - d0, this.player.posY - d1,
+	 * this.player.posZ - d2);
+	 * 
+	 * if (!this.player.noClip && !this.player.isPlayerSleeping()) { boolean flag1 =
+	 * worldserver.getCollisionBoxes(this.player,
+	 * this.player.getEntityBoundingBox().shrink(0.0625D)).isEmpty();
+	 * 
+	 * if (flag2 && (flag || !flag1)) { this.setPlayerLocation(d0, d1, d2, f, f1);
+	 * return; } }
+	 * 
+	 * this.floating = d12 >= -0.03125D; this.floating &=
+	 * !this.serverController.isFlightAllowed() &&
+	 * !this.player.capabilities.allowFlying; this.floating &=
+	 * !this.player.isPotionActive(MobEffects.LEVITATION) &&
+	 * !this.player.isElytraFlying() &&
+	 * !worldserver.checkBlockCollision(this.player.getEntityBoundingBox().grow(0.
+	 * 0625D).expand(0.0D, -0.55D, 0.0D)); this.player.onGround =
+	 * packetIn.isOnGround();
+	 * this.serverController.getPlayerList().serverUpdateMovingPlayer(this.player);
+	 * this.player.handleFalling(this.player.posY - d3, packetIn.isOnGround());
+	 * this.lastGoodX = this.player.posX; this.lastGoodY = this.player.posY;
+	 * this.lastGoodZ = this.player.posZ; } } } } } }
+	 */
+
 	/**
 	 * Inject this before the processPlayer() method gets called to move the player
 	 * and to transform the position of the packet as well.
@@ -103,63 +352,71 @@ public abstract class MixinNetHandlerPlayServer {
 	 * @param info
 	 */
 	/*
-	@Inject(method = "processPlayer", at = @At("HEAD"))
-	private void preProcessPlayer(CPacketPlayer packetIn, CallbackInfo info) {
-		WorldServer worldserver = this.serverController.getWorld(this.player.dimension);
-		IThreadListener listener = worldserver;
-		if (listener.isCallingFromMinecraftThread()) {
-			PhysicsWrapperEntity worldBelow = IDraggable.class.cast(this.player).getForcedSubspaceBelowFeet();
-			if (worldBelow != null) {
-				ISubspacedEntityRecord record = worldBelow.getPhysicsObject().getSubspace()
-						.getRecordForSubspacedEntity(ISubspacedEntity.class.cast(this.player));
-				if (record != null) {
-					System.out.println("Test");
-					VectorImmutable position = record.getPositionInGlobalCoordinates();
-					VectorImmutable lookDirection = record.getPositionInGlobalCoordinates();
-					VectorImmutable velocity = record.getVelocityInGlobalCoordinates();
-					// ===== HACKY STUFF STARTS HERE =====
-					this.firstGoodX = this.lastGoodX = position.getX();
-					this.firstGoodY = this.lastGoodY = position.getY();
-					this.firstGoodZ = this.lastGoodZ = position.getZ();
+	 * @Inject(method = "processPlayer", at = @At("HEAD")) private void
+	 * preProcessPlayer(CPacketPlayer packetIn, CallbackInfo info) { WorldServer
+	 * worldserver = this.serverController.getWorld(this.player.dimension);
+	 * IThreadListener listener = worldserver; if
+	 * (listener.isCallingFromMinecraftThread()) { PhysicsWrapperEntity worldBelow =
+	 * IDraggable.class.cast(this.player).getForcedSubspaceBelowFeet(); if
+	 * (worldBelow != null) { ISubspacedEntityRecord record =
+	 * worldBelow.getPhysicsObject().getSubspace()
+	 * .getRecordForSubspacedEntity(ISubspacedEntity.class.cast(this.player)); if
+	 * (record != null) { VectorImmutable position =
+	 * record.getPositionInGlobalCoordinates(); VectorImmutable lookDirection =
+	 * record.getPositionInGlobalCoordinates(); VectorImmutable velocity =
+	 * record.getVelocityInGlobalCoordinates();
+	 * 
+	 * Vector positionMutable = position.createMutibleVectorCopy(); Vector
+	 * playerPosition = new Vector(player);
+	 * positionMutable.subtract(playerPosition); if (positionMutable.lengthSq() >
+	 * 1000 || isVectorInvalid(position) || isVectorInvalid(lookDirection) ||
+	 * isVectorInvalid(velocity)) { // Bad packet, ignore it. } else { // =====
+	 * HACKY STUFF STARTS HERE ===== this.firstGoodX = this.lastGoodX =
+	 * position.getX(); this.firstGoodY = this.lastGoodY = position.getY();
+	 * this.firstGoodZ = this.lastGoodZ = position.getZ();
+	 * 
+	 * float pitch = (float) VWMath.getPitchFromVectorImmutable(lookDirection);
+	 * float yaw = (float) VWMath.getYawFromVectorImmutable(lookDirection, pitch);
+	 * 
+	 * // this.player.setPositionAndRotation(position.getX(), position.getY(), //
+	 * position.getZ(), yaw, // pitch);
+	 * 
+	 * this.player.motionX = position.getX() - player.posX; this.player.motionY =
+	 * position.getY() - player.posY; this.player.motionZ = position.getZ() -
+	 * player.posZ;
+	 * 
+	 * // ===== NOW FIX THE PACKET ===== // CPacketPlayer fixed = new
+	 * CPacketPlayer(); packetIn.x = position.getX(); packetIn.y = position.getY();
+	 * packetIn.z = position.getZ(); packetIn.moving = true; packetIn.onGround =
+	 * true; // packetIn.rotating = true; // packetIn.pitch = pitch; // packetIn.yaw
+	 * = yaw;
+	 * 
+	 * // player.setPositionAndRotation(position.getX(), position.getY(), //
+	 * position.getZ(), yaw, pitch);
+	 * 
+	 * System.out.println("Used one");
+	 * 
+	 * // packetIn.onGround = true; // packetIn.pitch = pitch; // packetIn.yaw =
+	 * yaw; }
+	 * 
+	 * worldBelow.getPhysicsObject().getSubspace()
+	 * .forceSubspaceRecord(ISubspacedEntity.class.cast(this.player), null); } } } }
+	 */
 
-					float pitch = (float) VWMath.getPitchFromVectorImmutable(lookDirection);
-					float yaw = (float) VWMath.getYawFromVectorImmutable(lookDirection, pitch);
-
-					this.player.setPositionAndRotation(position.getX(), position.getY(), position.getZ(), yaw, pitch);
-
-					this.player.motionX = velocity.getX();
-					this.player.motionY = velocity.getY();
-					this.player.motionZ = velocity.getZ();
-
-					// ===== NOW FIX THE PACKET =====
-					// CPacketPlayer fixed = new CPacketPlayer();
-					packetIn.x = position.getX();
-					packetIn.y = position.getY();
-					packetIn.z = position.getZ();
-					packetIn.moving = true;
-					packetIn.onGround = true;
-					packetIn.pitch = pitch;
-					packetIn.yaw = yaw;
-					
-					IDraggable.class.cast(this.player).setForcedRelativeSubspace(null);
-				}
-			}
-		}
-	}
-	*/
-
-	@Redirect(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetHandlerPlayServer;captureCurrentPosition()V"))
-	public void makeIDraggableNotUseless(NetHandlerPlayServer server) {
-		IDraggable draggable = EntityDraggable.getDraggableFromEntity(player);
-		server.captureCurrentPosition();
-
-		server.firstGoodX += draggable.getVelocityAddedToPlayer().X;
-		server.firstGoodY += draggable.getVelocityAddedToPlayer().Y;
-		server.firstGoodZ += draggable.getVelocityAddedToPlayer().Z;
-		server.lastGoodX += draggable.getVelocityAddedToPlayer().X;
-		server.lastGoodY += draggable.getVelocityAddedToPlayer().Y;
-		server.lastGoodZ += draggable.getVelocityAddedToPlayer().Z;
-	}
+	/*
+	 * @Redirect(method = "update", at = @At(value = "INVOKE", target =
+	 * "Lnet/minecraft/network/NetHandlerPlayServer;captureCurrentPosition()V"))
+	 * public void makeIDraggableNotUseless(NetHandlerPlayServer server) {
+	 * IDraggable draggable = EntityDraggable.getDraggableFromEntity(player);
+	 * server.captureCurrentPosition();
+	 * 
+	 * server.firstGoodX += draggable.getVelocityAddedToPlayer().X;
+	 * server.firstGoodY += draggable.getVelocityAddedToPlayer().Y;
+	 * server.firstGoodZ += draggable.getVelocityAddedToPlayer().Z; server.lastGoodX
+	 * += draggable.getVelocityAddedToPlayer().X; server.lastGoodY +=
+	 * draggable.getVelocityAddedToPlayer().Y; server.lastGoodZ +=
+	 * draggable.getVelocityAddedToPlayer().Z; }
+	 */
 
 	@Shadow
 	public void disconnect(final ITextComponent textComponent) {

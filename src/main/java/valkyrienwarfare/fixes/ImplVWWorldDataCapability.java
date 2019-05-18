c81335e9957a7e031870c905deb2b6b1efb81190
Alexander Mastrangelo <best108@uw.edu>
2019-05-17 23:53:09 -0700

    Added basic chunk allocation functionality to the capability.

--- src/main/java/valkyrienwarfare/fixes/ImplVWWorldDataCapability.java
+++ src/main/java/valkyrienwarfare/fixes/ImplVWWorldDataCapability.java
@@ -5,18 +5,21 @@ import net.minecraft.world.World;
 
 public class ImplVWWorldDataCapability implements IVWWorldDataCapability {
 
+    // The world this capability belongs to.
     private World world;
+    private final ShipChunkAllocator shipChunkAllocator = new ShipChunkAllocator();
 
     @Override
     public NBTTagCompound writeToNBT() {
         NBTTagCompound toReturn = new NBTTagCompound();
-        // TODO: Actually write something here
+        shipChunkAllocator.writeToNBT(toReturn);
         return toReturn;
     }
 
     @Override
     public void readFromNBT(NBTTagCompound compound) {
         // System.out.println("Reading cap crap from disk!");
+        shipChunkAllocator.readFromNBT(compound);
     }
 
     @Override

d2964a414682196b6d291922557b10b6f0d46582
Rubydesic <rubydesic@gmail.com>
2019-10-12 02:32:00 -0400

    Add tab-autocompletion

--- src/main/java/org/valkyrienskies/mod/common/command/framework/VSCommandBase.java
+++ src/main/java/org/valkyrienskies/mod/common/command/framework/VSCommandBase.java
@@ -1,19 +1,26 @@
 package org.valkyrienskies.mod.common.command.framework;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+import lombok.SneakyThrows;
 import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import org.apache.commons.io.output.NullOutputStream;
 import org.valkyrienskies.mod.common.command.converters.WorldConverter;
+import picocli.AutoComplete;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Model.CommandSpec;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
@@ -37,6 +44,29 @@ public class VSCommandBase<K> extends CommandBase {
     @Override
     public List<String> getAliases() {
         return Arrays.asList(this.cmdClass.getAnnotation(Command.class).aliases());
+    }
+
+    @SneakyThrows
+    @Override
+    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender,
+        final String[] splitArgs, @Nullable BlockPos targetPos) {
+
+        CommandSpec spec = CommandSpec.forAnnotatedObject(cmdClass.newInstance());
+        String[] args = VSCommandUtil.toProperArgs(splitArgs);
+        List<CharSequence> candidates = new ArrayList<>();
+
+        if (args.length == 0) {
+            AutoComplete.complete(spec, args, 0, 0, 500, candidates);
+        } else {
+            AutoComplete.complete(spec, args, args.length - 1,
+                args[args.length - 1].length(), 500, candidates);
+        }
+
+        return candidates.stream()
+            .distinct()
+            .map(CharSequence::toString)
+            .map(s -> args.length > 0 ? args[args.length - 1] + s : s)
+            .collect(Collectors.toList());
     }
 
     @Override
@@ -70,6 +100,11 @@ public class VSCommandBase<K> extends CommandBase {
         ChatWriter(ICommandSender sender) {
             super(new NullOutputStream());
             this.sender = sender;
+        }
+
+        @Override
+        public void print(Object object) {
+            this.print(object.toString());
         }
 
         @Override

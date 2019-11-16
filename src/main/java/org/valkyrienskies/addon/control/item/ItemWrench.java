7bed080bb2c06302679617dd5fde3412a0194977
DeltaNedas <39013340+DeltaNedas@users.noreply.github.com>
2019-11-16 18:54:22 +0000

    start on more wrench mode updates

--- src/main/java/org/valkyrienskies/addon/control/item/ItemWrench.java
+++ src/main/java/org/valkyrienskies/addon/control/item/ItemWrench.java
@@ -39,8 +39,12 @@ public class ItemWrench extends Item {
     public void addInformation(ItemStack stack, @Nullable World player,
         List<String> itemInformation,
         ITooltipFlag advanced) {
-        itemInformation.add(TextFormatting.BLUE + I18n.format("tooltip.vs_control.wrench." + this.mode.toString()));
-        itemInformation.add(TextFormatting.GREEN + "" + TextFormatting.ITALIC + I18n.format("tooltip.vs_control.wrench_modes"));
+        if (VSConfig.wrenchModeless) {
+            itemInformation.add(TextFormatting.BLUE + I18n.format("tooltip.vs_control.wrench_toggle"));
+        } else {
+            itemInformation.add(TextFormatting.BLUE + I18n.format("tooltip.vs_control.wrench." + this.mode.toString()));
+            itemInformation.add(TextFormatting.GREEN + "" + TextFormatting.ITALIC + I18n.format("tooltip.vs_control.wrench_modes"));
+        }
     }
 
     // Construct potential multiblock if set to construct mode.
@@ -53,6 +57,7 @@ public class ItemWrench extends Item {
             return EnumActionResult.SUCCESS;
         }
 
+        if (VSConfig.wrenchModeless) {
         if (player.isSneaking()) {
             this.mode = EnumWrenchMode.values()[(this.mode.ordinal() + 1) % EnumWrenchMode.values().length]; // Switch to the next mode
             player.sendMessage(new TextComponentString(

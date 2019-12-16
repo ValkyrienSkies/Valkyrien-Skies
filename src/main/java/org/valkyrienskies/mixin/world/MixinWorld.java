ValkyrienSkies/Valkyrien-Skies

commit 9805dd0c38190552727fd856f69bd0cbf8327afc

Replaced some mixin injections with forge events.

src/main/java/org/valkyrienskies/mixin/world/MixinWorld.java
@@ -22,6 +22,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 import javax.annotation.Nullable;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
@@ -56,16 +57,18 @@ import org.valkyrienskies.mod.common.physics.collision.polygons.Polygon;
 import org.valkyrienskies.mod.common.physics.management.PhysicsObject;
 import org.valkyrienskies.mod.common.physics.management.WorldPhysObjectManager;
 import org.valkyrienskies.mod.common.physmanagement.interaction.IWorldVW;
+import org.valkyrienskies.mod.common.ship_handling.IHasShipManager;
+import org.valkyrienskies.mod.common.ship_handling.IWorldShipManager;
 import org.valkyrienskies.mod.common.util.ValkyrienUtils;
 import valkyrienwarfare.api.TransformType;
 
 // TODO: This class is horrible
 @Mixin(value = World.class, priority = 2018)
 @Implements(@Interface(iface = MixinWorldIntrinsicMethods.class, prefix = "vw$", remap = Remap.NONE))
-public abstract class MixinWorld implements IWorldVW, ISubspaceProvider {
+public abstract class MixinWorld implements IWorldVW, ISubspaceProvider, IHasShipManager {
 
-    private static double MAX_ENTITY_RADIUS_ALT = 2.0D;
-    private static double BOUNDING_BOX_EDGE_LIMIT = 100000D;
+    private static final double MAX_ENTITY_RADIUS_ALT = 2.0D;
+    private static final double BOUNDING_BOX_EDGE_LIMIT = 100000D;
     // TODO: This is going to lead to a multithreaded disaster. Replace this with something sensible!
     // I made this threadlocal to prevent disaster for now, but its still really bad code.
     private final ThreadLocal<Boolean> dontIntercept = ThreadLocal.withInitial(() -> false);
@@ -73,6 +76,9 @@ public abstract class MixinWorld implements IWorldVW, ISubspaceProvider {
     private final ThreadLocal<PhysicsWrapperEntity> dontInterceptShip = ThreadLocal
         .withInitial(() -> null);
     private final ISubspace worldSubspace = new ImplSubspace(null);
+    private final World world = World.class.cast(this);
+    // The IWorldShipManager
+    private IWorldShipManager manager = null;
 
     @Shadow
     protected List<IWorldEventListener> eventListeners;
@@ -91,7 +97,7 @@ public abstract class MixinWorld implements IWorldVW, ISubspaceProvider {
     @Intrinsic(displace = true)
     public Biome vw$getBiomeForCoordsBody(BlockPos pos) {
         Optional<PhysicsObject> physicsObject = ValkyrienUtils
-            .getPhysicsObject(World.class.cast(this), pos);
+            .getPhysicsObject(world, pos);
 
         if (physicsObject.isPresent()) {
             pos = physicsObject.get()
@@ -118,7 +124,7 @@ public abstract class MixinWorld implements IWorldVW, ISubspaceProvider {
         double ySpeed, double zSpeed, int... parameters) {
         BlockPos pos = new BlockPos(x, y, z);
         Optional<PhysicsObject> physicsObject = ValkyrienUtils
-            .getPhysicsObject(World.class.cast(this), pos);
+            .getPhysicsObject(world, pos);
 
         if (physicsObject.isPresent()) {
             Vector newPosVec = new Vector(x, y, z);
@@ -230,7 +236,7 @@ public abstract class MixinWorld implements IWorldVW, ISubspaceProvider {
         BlockPos pos = new BlockPos((aabb.minX + aabb.maxX) / 2D, (aabb.minY + aabb.maxY) / 2D,
             (aabb.minZ + aabb.maxZ) / 2D);
         Optional<PhysicsObject> physicsObject = ValkyrienUtils
-            .getPhysicsObject(World.class.cast(this), pos);
+            .getPhysicsObject(world, pos);
 
         if (physicsObject.isPresent()) {
             Polygon poly = new Polygon(aabb, physicsObject.get()
@@ -290,7 +296,7 @@ public abstract class MixinWorld implements IWorldVW, ISubspaceProvider {
             (boundingBox.minY + boundingBox.maxY) / 2D, (boundingBox.minZ + boundingBox.maxZ) / 2D);
 
         Optional<PhysicsObject> physicsObject = ValkyrienUtils
-            .getPhysicsObject(World.class.cast(this), pos);
+            .getPhysicsObject(world, pos);
 
         if (physicsObject.isPresent()) {
             Polygon poly = new Polygon(boundingBox, physicsObject.get()
@@ -362,11 +368,11 @@ public abstract class MixinWorld implements IWorldVW, ISubspaceProvider {
         boolean ignoreBlockWithoutBoundingBox, boolean returnLastUncollidableBlock,
         PhysicsWrapperEntity toIgnore) {
         this.dontIntercept.set(true);
-        RayTraceResult vanillaTrace = World.class.cast(this)
+        RayTraceResult vanillaTrace = world
             .rayTraceBlocks(vec31, vec32, stopOnLiquid,
                 ignoreBlockWithoutBoundingBox, returnLastUncollidableBlock);
         WorldPhysObjectManager physManager = ValkyrienSkiesMod.VW_PHYSICS_MANAGER
-            .getManagerForWorld(World.class.cast(this));
+            .getManagerForWorld(world);
         if (physManager == null) {
             return vanillaTrace;
         }
@@ -399,7 +405,7 @@ public abstract class MixinWorld implements IWorldVW, ISubspaceProvider {
 
             Vec3d playerEyesReachAdded = playerEyesPos.add(playerReachVector.x * reachDistance,
                 playerReachVector.y * reachDistance, playerReachVector.z * reachDistance);
-            RayTraceResult resultInShip = World.class.cast(this)
+            RayTraceResult resultInShip = world
                 .rayTraceBlocks(playerEyesPos, playerEyesReachAdded,
                     stopOnLiquid, ignoreBlockWithoutBoundingBox, returnLastUncollidableBlock);
             if (resultInShip != null && resultInShip.hitVec != null
@@ -419,4 +425,19 @@ public abstract class MixinWorld implements IWorldVW, ISubspaceProvider {
         this.dontIntercept.set(false);
         return vanillaTrace;
     }
+
+    @Override
+    public IWorldShipManager getManager() {
+        if (manager == null) {
+            throw new IllegalStateException(
+                "We can't be accessing this manager since WorldEvent.load() was never called!");
+        }
+        return manager;
+    }
+
+    @Override
+    public void setManager(Function<World, IWorldShipManager> managerSupplier) {
+        manager = managerSupplier.apply(world);
+    }
+
 }

src/main/java/org/valkyrienskies/mixin/world/MixinClientWorld.java
@@ -1,0 +1,81 @@
+package org.valkyrienskies.mixin.world;
+
+import java.util.List;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.EnumSkyBlock;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.valkyrienskies.mod.common.ValkyrienSkiesMod;
+import org.valkyrienskies.mod.common.entity.PhysicsWrapperEntity;
+import valkyrienwarfare.api.TransformType;
+
+/**
+ * This class contains the mixins for the World class that are client side only.
+ */
+@Mixin(World.class)
+public class MixinClientWorld {
+
+    private final World world = World.class.cast(this);
+
+    @Inject(method = "getCombinedLight(Lnet/minecraft/util/math/BlockPos;I)I", at = @At("HEAD"),
+        cancellable = true)
+    private void preGetCombinedLight(BlockPos pos, int lightValue,
+        CallbackInfoReturnable<Integer> callbackInfoReturnable) {
+        try {
+            int i = world.getLightFromNeighborsFor(EnumSkyBlock.SKY, pos);
+            int j = world.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, pos);
+            AxisAlignedBB lightBB = new AxisAlignedBB(pos.getX() - 2, pos.getY() - 2,
+                pos.getZ() - 2, pos.getX() + 2, pos.getY() + 2, pos.getZ() + 2);
+            List<PhysicsWrapperEntity> physEnts = ValkyrienSkiesMod.VW_PHYSICS_MANAGER
+                .getManagerForWorld(world).getNearbyPhysObjects(lightBB);
+
+            for (PhysicsWrapperEntity physEnt : physEnts) {
+                BlockPos posInLocal = physEnt.getPhysicsObject().getShipTransformationManager()
+                    .getCurrentTickTransform().transform(pos, TransformType.GLOBAL_TO_SUBSPACE);
+                int localI = world.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal);
+                int localJ = world.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal);
+                if (localI == 0 && localJ == 0) {
+                    localI = world.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal.up());
+                    localJ = world.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal.up());
+                }
+                if (localI == 0 && localJ == 0) {
+                    localI = world.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal.down());
+                    localJ = world.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal.down());
+                }
+                if (localI == 0 && localJ == 0) {
+                    localI = world.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal.north());
+                    localJ = world.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal.north());
+                }
+                if (localI == 0 && localJ == 0) {
+                    localI = world.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal.south());
+                    localJ = world.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal.south());
+                }
+                if (localI == 0 && localJ == 0) {
+                    localI = world.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal.east());
+                    localJ = world.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal.east());
+                }
+                if (localI == 0 && localJ == 0) {
+                    localI = world.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal.west());
+                    localJ = world.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal.west());
+                }
+
+                i = Math.min(localI, i);
+                j = Math.max(localJ, j);
+            }
+
+            if (j < lightValue) {
+                j = lightValue;
+            }
+
+            callbackInfoReturnable.setReturnValue(i << 20 | j << 4);
+        } catch (Exception e) {
+            System.err
+                .println("Something just went wrong here, getting default light value instead!");
+            e.printStackTrace();
+        }
+    }
+}

src/main/java/org/valkyrienskies/mixin/world/MixinWorldClient.java
@@ -1,118 +1,0 @@
-/*
- * Adapted from the Wizardry License
- *
- * Copyright (c) 2015-2018 the Valkyrien Warfare team
- *
- * Permission is hereby granted to any persons and/or organizations using this software to copy, modify, merge, publish, and distribute it.
- * Said persons and/or organizations are not allowed to use the software or any derivatives of the work for commercial use or any other means to generate income unless it is to be used as a part of a larger project (IE: "modpacks"), nor are they allowed to claim this software as their own.
- *
- * The persons and/or organizations are also disallowed from sub-licensing and/or trademarking this software without explicit permission from the Valkyrien Warfare team.
- *
- * Any persons and/or organizations using this software must disclose their source code and have it publicly available, include this license, provide sufficient credit to the original authors of the project (IE: The Valkyrien Warfare team), as well as provide a link to the original project.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- */
-
-package org.valkyrienskies.mixin.world;
-
-import java.util.List;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.EnumSkyBlock;
-import net.minecraft.world.World;
-import net.minecraft.world.chunk.Chunk;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.valkyrienskies.mod.common.ValkyrienSkiesMod;
-import org.valkyrienskies.mod.common.entity.PhysicsWrapperEntity;
-import org.valkyrienskies.mod.common.ship_handling.IHasShipManager;
-import org.valkyrienskies.mod.common.ship_handling.WorldClientShipManager;
-import valkyrienwarfare.api.TransformType;
-
-@Mixin(World.class)
-public abstract class MixinWorldClient implements IHasShipManager {
-
-    private final WorldClientShipManager manager = new WorldClientShipManager(
-        World.class.cast(this));
-
-    @Inject(method = "tick", at = @At("RETURN"))
-    private void onTickPost(CallbackInfo callbackInfo) {
-        manager.tick();
-    }
-
-    @Shadow
-    public abstract int getLightFromNeighborsFor(EnumSkyBlock type, BlockPos pos);
-
-    @Shadow
-    public abstract Chunk getChunk(BlockPos pos);
-
-    @Inject(method = "getCombinedLight(Lnet/minecraft/util/math/BlockPos;I)I", at = @At("HEAD"), cancellable = true)
-    private void preGetCombinedLight(BlockPos pos, int lightValue,
-        CallbackInfoReturnable callbackInfoReturnable) {
-        try {
-            int i = this.getLightFromNeighborsFor(EnumSkyBlock.SKY, pos);
-            int j = this.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, pos);
-            AxisAlignedBB lightBB = new AxisAlignedBB(pos.getX() - 2, pos.getY() - 2,
-                pos.getZ() - 2, pos.getX() + 2, pos.getY() + 2, pos.getZ() + 2);
-            List<PhysicsWrapperEntity> physEnts = ValkyrienSkiesMod.VW_PHYSICS_MANAGER
-                .getManagerForWorld(World.class.cast(this)).getNearbyPhysObjects(lightBB);
-
-            for (PhysicsWrapperEntity physEnt : physEnts) {
-//                BlockPos posInLocal = RotationMatrices.applyTransform(physEnt.wrapping.coordTransform.wToLTransform, pos);
-                BlockPos posInLocal = physEnt.getPhysicsObject().getShipTransformationManager()
-                    .getCurrentTickTransform().transform(pos, TransformType.GLOBAL_TO_SUBSPACE);
-                int localI = this.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal);
-                int localJ = this.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal);
-                if (localI == 0 && localJ == 0) {
-                    localI = this.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal.up());
-                    localJ = this.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal.up());
-                }
-                if (localI == 0 && localJ == 0) {
-                    localI = this.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal.down());
-                    localJ = this.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal.down());
-                }
-                if (localI == 0 && localJ == 0) {
-                    localI = this.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal.north());
-                    localJ = this.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal.north());
-                }
-                if (localI == 0 && localJ == 0) {
-                    localI = this.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal.south());
-                    localJ = this.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal.south());
-                }
-                if (localI == 0 && localJ == 0) {
-                    localI = this.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal.east());
-                    localJ = this.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal.east());
-                }
-                if (localI == 0 && localJ == 0) {
-                    localI = this.getLightFromNeighborsFor(EnumSkyBlock.SKY, posInLocal.west());
-                    localJ = this.getLightFromNeighborsFor(EnumSkyBlock.BLOCK, posInLocal.west());
-                }
-
-                i = Math.min(localI, i);
-                j = Math.max(localJ, j);
-            }
-
-            if (j < lightValue) {
-                j = lightValue;
-            }
-
-            callbackInfoReturnable.setReturnValue(i << 20 | j << 4);
-            callbackInfoReturnable.cancel();
-            return;
-        } catch (Exception e) {
-            System.err
-                .println("Something just went wrong here, getting default light value instead!!!!");
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public WorldClientShipManager getManager() {
-        return manager;
-    }
-}

src/main/java/org/valkyrienskies/mixin/world/MixinWorldServer.java
@@ -1,45 +1,0 @@
-package org.valkyrienskies.mixin.world;
-
-import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.valkyrienskies.mod.common.ship_handling.IHasShipManager;
-import org.valkyrienskies.mod.common.ship_handling.WorldServerShipManager;
-
-/**
- * More lazy programming. This can DEFINITELY be replaced with forge capabilities and events!
- */
-@Deprecated
-@Mixin(value = WorldServer.class)
-public class MixinWorldServer implements IHasShipManager {
-
-    private WorldServerShipManager manager = null;
-
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void onConstructorFinished(CallbackInfo callbackInfo) {
-        // if (PorkDB.contains(worldid) {
-        // manager = PorkDB.load(worldid)
-        // } else {
-        manager = new WorldServerShipManager();
-        // }
-        manager.initializeTransients(World.class.cast(this));
-    }
-
-    @Inject(method = "flush", at = @At("HEAD"))
-    private void beforeWorldUnload(CallbackInfo callbackInfo) {
-        manager.onWorldUnload();
-    }
-
-    @Inject(method = "tick", at = @At("RETURN"))
-    private void onTickPost(CallbackInfo callbackInfo) {
-        manager.tick();
-    }
-
-    @Override
-    public WorldServerShipManager getManager() {
-        return manager;
-    }
-}

src/main/java/org/valkyrienskies/mod/client/EventsClient.java
@@ -28,6 +28,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
 import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.event.TextureStitchEvent;
@@ -49,6 +50,7 @@ import org.valkyrienskies.mod.common.math.Vector;
 import org.valkyrienskies.mod.common.physics.management.PhysicsObject;
 import org.valkyrienskies.mod.common.physics.management.WorldPhysObjectManager;
 import org.valkyrienskies.mod.common.physmanagement.interaction.EntityDraggable;
+import org.valkyrienskies.mod.common.ship_handling.IHasShipManager;
 import org.valkyrienskies.mod.common.util.ValkyrienUtils;
 import valkyrienwarfare.api.TransformType;
 
@@ -62,6 +64,26 @@ public class EventsClient {
         if (entity == Minecraft.getMinecraft().player) {
             Minecraft.getMinecraft().entityRenderer
                 .getMouseOver(Minecraft.getMinecraft().getRenderPartialTicks());
+        }
+    }
+
+    @SubscribeEvent
+    public void onClientTick(ClientTickEvent event) {
+        World world = Minecraft.getMinecraft().world;
+        if (world == null) {
+            // There's no world, so there's nothing to run.
+            return;
+        }
+        // Pretend this is the world tick, because diesieben07 doesn't want WorldClient to make world tick events.
+        switch (event.phase) {
+            case START:
+                // Nothing for now
+                break;
+            case END:
+                // Tick the IShipManager on the world client.
+                IHasShipManager shipManager = (IHasShipManager) world;
+                shipManager.getManager().tick();
+                break;
         }
     }
 
@@ -110,25 +132,23 @@ public class EventsClient {
     public void onDrawBlockHighlightEventFirst(DrawBlockHighlightEvent event) {
         GL11.glPushMatrix();
         BlockPos pos = Minecraft.getMinecraft().objectMouseOver.getBlockPos();
-        if (pos != null) {
-            Optional<PhysicsObject> physicsObject = ValkyrienUtils
-                .getPhysicsObject(Minecraft.getMinecraft().world, pos);
-            if (physicsObject.isPresent()) {
-                RayTraceResult objectOver = Minecraft.getMinecraft().objectMouseOver;
-                if (objectOver != null && objectOver.hitVec != null) {
-                    BufferBuilder buffer = Tessellator.getInstance().getBuffer();
-                    oldXOff = buffer.xOffset;
-                    oldYOff = buffer.yOffset;
-                    oldZOff = buffer.zOffset;
+        Optional<PhysicsObject> physicsObject = ValkyrienUtils
+            .getPhysicsObject(Minecraft.getMinecraft().world, pos);
+        if (physicsObject.isPresent()) {
+            RayTraceResult objectOver = Minecraft.getMinecraft().objectMouseOver;
+            if (objectOver != null && objectOver.hitVec != null) {
+                BufferBuilder buffer = Tessellator.getInstance().getBuffer();
+                oldXOff = buffer.xOffset;
+                oldYOff = buffer.yOffset;
+                oldZOff = buffer.zOffset;
 
-                    buffer.setTranslation(-physicsObject.get()
-                        .getShipRenderer().offsetPos.getX(), -physicsObject.get()
-                        .getShipRenderer().offsetPos.getY(), -physicsObject.get()
-                        .getShipRenderer().offsetPos.getZ());
-                    physicsObject.get()
-                        .getShipRenderer()
-                        .setupTranslation(event.getPartialTicks());
-                }
+                buffer.setTranslation(-physicsObject.get()
+                    .getShipRenderer().offsetPos.getX(), -physicsObject.get()
+                    .getShipRenderer().offsetPos.getY(), -physicsObject.get()
+                    .getShipRenderer().offsetPos.getZ());
+                physicsObject.get()
+                    .getShipRenderer()
+                    .setupTranslation(event.getPartialTicks());
             }
         }
     }
@@ -136,19 +156,17 @@ public class EventsClient {
     @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
     public void onDrawBlockHighlightEventLast(DrawBlockHighlightEvent event) {
         BlockPos pos = Minecraft.getMinecraft().objectMouseOver.getBlockPos();
-        if (pos != null) {
-            Optional<PhysicsObject> physicsObject = ValkyrienUtils
-                .getPhysicsObject(Minecraft.getMinecraft().world, pos);
-            if (physicsObject.isPresent()) {
-                RayTraceResult objectOver = Minecraft.getMinecraft().objectMouseOver;
-                if (objectOver != null && objectOver.hitVec != null) {
-                    BufferBuilder buffer = Tessellator.getInstance().getBuffer();
-                    buffer.xOffset = oldXOff;
-                    buffer.yOffset = oldYOff;
-                    buffer.zOffset = oldZOff;
-                    // wrapper.wrapping.renderer.inverseTransform(event.getPartialTicks());
-                    // objectOver.hitVec = RotationMatrices.applyTransform(wrapper.wrapping.coordTransform.lToWTransform, objectOver.hitVec);
-                }
+        Optional<PhysicsObject> physicsObject = ValkyrienUtils
+            .getPhysicsObject(Minecraft.getMinecraft().world, pos);
+        if (physicsObject.isPresent()) {
+            RayTraceResult objectOver = Minecraft.getMinecraft().objectMouseOver;
+            if (objectOver != null && objectOver.hitVec != null) {
+                BufferBuilder buffer = Tessellator.getInstance().getBuffer();
+                buffer.xOffset = oldXOff;
+                buffer.yOffset = oldYOff;
+                buffer.zOffset = oldZOff;
+                // wrapper.wrapping.renderer.inverseTransform(event.getPartialTicks());
+                // objectOver.hitVec = RotationMatrices.applyTransform(wrapper.wrapping.coordTransform.lToWTransform, objectOver.hitVec);
             }
         }
         GL11.glPopMatrix();

commit accf188c8a48bad6f90eb5327a87b8547fbed574

cherrypick Trying to fix a regression that prevents 'muhist' Forge mod from begin compatible
caused by regression in 9805dd0c38190552727fd856f69bd0cbf8327afc

src/main/java/org/valkyrienskies/mixin/world/MixinWorld.java
@@ -79,7 +79,7 @@ public abstract class MixinWorld implements IWorldVS, ISubspaceProvider, IHasShi
     // Pork added on to this already bad code because it was already like this so he doesn't feel bad about it
     private PhysicsWrapperEntity dontInterceptShip = null;
     private final ISubspace worldSubspace = new ImplSubspace(null);
-    private final World world = World.class.cast(this);
+
     // The IWorldShipManager
     private IWorldShipManager manager = null;
     // Rotation Node World fields. Note this is only used in multiplayer, but making a MixinWorldServer
@@ -103,7 +103,7 @@ public abstract class MixinWorld implements IWorldVS, ISubspaceProvider, IHasShi
     @Intrinsic(displace = true)
     public Biome vs$getBiomeForCoordsBody(BlockPos pos) {
         Optional<PhysicsObject> physicsObject = ValkyrienUtils
-            .getPhysicsObject(world, pos);
+            .getPhysicsObject(World.class.cast(this), pos);
 
         if (physicsObject.isPresent()) {
             pos = physicsObject.get()
@@ -142,7 +142,7 @@ public abstract class MixinWorld implements IWorldVS, ISubspaceProvider, IHasShi
         double ySpeed, double zSpeed, int... parameters) {
         BlockPos pos = new BlockPos(x, y, z);
         Optional<PhysicsObject> physicsObject = ValkyrienUtils
-            .getPhysicsObject(world, pos);
+            .getPhysicsObject(World.class.cast(this), pos);
 
         if (physicsObject.isPresent()) {
             Vector newPosVec = new Vector(x, y, z);
@@ -254,7 +254,7 @@ public abstract class MixinWorld implements IWorldVS, ISubspaceProvider, IHasShi
         BlockPos pos = new BlockPos((aabb.minX + aabb.maxX) / 2D, (aabb.minY + aabb.maxY) / 2D,
             (aabb.minZ + aabb.maxZ) / 2D);
         Optional<PhysicsObject> physicsObject = ValkyrienUtils
-            .getPhysicsObject(world, pos);
+            .getPhysicsObject(World.class.cast(this), pos);
 
         if (physicsObject.isPresent()) {
             Polygon poly = new Polygon(aabb, physicsObject.get()
@@ -293,7 +293,7 @@ public abstract class MixinWorld implements IWorldVS, ISubspaceProvider, IHasShi
             (boundingBox.minY + boundingBox.maxY) / 2D, (boundingBox.minZ + boundingBox.maxZ) / 2D);
 
         Optional<PhysicsObject> physicsObject = ValkyrienUtils
-            .getPhysicsObject(world, pos);
+            .getPhysicsObject(World.class.cast(this), pos);
 
         if (physicsObject.isPresent()) {
             Polygon poly = new Polygon(boundingBox, physicsObject.get()
@@ -365,11 +365,11 @@ public abstract class MixinWorld implements IWorldVS, ISubspaceProvider, IHasShi
         boolean ignoreBlockWithoutBoundingBox, boolean returnLastUncollidableBlock,
         PhysicsWrapperEntity toIgnore) {
         this.dontIntercept = true;
-        RayTraceResult vanillaTrace = world
+        RayTraceResult vanillaTrace = World.class.cast(this)
             .rayTraceBlocks(vec31, vec32, stopOnLiquid,
                 ignoreBlockWithoutBoundingBox, returnLastUncollidableBlock);
         WorldPhysObjectManager physManager = ValkyrienSkiesMod.VS_PHYSICS_MANAGER
-            .getManagerForWorld(world);
+            .getManagerForWorld(World.class.cast(this));
         if (physManager == null) {
             return vanillaTrace;
         }
@@ -403,7 +403,7 @@ public abstract class MixinWorld implements IWorldVS, ISubspaceProvider, IHasShi
 
             Vec3d playerEyesReachAdded = playerEyesPos.add(playerReachVector.x * reachDistance,
                 playerReachVector.y * reachDistance, playerReachVector.z * reachDistance);
-            RayTraceResult resultInShip = world
+            RayTraceResult resultInShip = World.class.cast(this)
                 .rayTraceBlocks(playerEyesPos, playerEyesReachAdded,
                     stopOnLiquid, ignoreBlockWithoutBoundingBox, returnLastUncollidableBlock);
             if (resultInShip != null && resultInShip.hitVec != null
@@ -434,7 +434,7 @@ public abstract class MixinWorld implements IWorldVS, ISubspaceProvider, IHasShi
 
     @Override
     public void setManager(Function<World, IWorldShipManager> managerSupplier) {
-        manager = managerSupplier.apply(world);
+        manager = managerSupplier.apply(World.class.cast(this));
     }
 
     @Override
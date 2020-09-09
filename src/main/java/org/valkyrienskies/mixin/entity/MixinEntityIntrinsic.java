5f9f6bb7b839c7f10029f1dbe823cc12c362d7b7
Tri0de <best108@uw.edu>
2020-09-09 03:09:10 -0700

    Cleaned up MixinEntityIntrinsic :wrench:

--- src/main/java/org/valkyrienskies/mixin/entity/MixinEntityIntrinsic.java
+++ src/main/java/org/valkyrienskies/mixin/entity/MixinEntityIntrinsic.java
@@ -10,9 +10,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.valkyrienskies.mod.common.entity.EntityShipMovementData;
-import org.valkyrienskies.mod.common.ships.entity_interaction.EntityMoveInjectionMethods;
 import org.valkyrienskies.mod.common.ships.entity_interaction.EntityCollisionInjector;
 import org.valkyrienskies.mod.common.ships.entity_interaction.EntityCollisionInjector.IntermediateMovementVariableStorage;
+import org.valkyrienskies.mod.common.ships.entity_interaction.EntityMoveInjectionMethods;
 import org.valkyrienskies.mod.common.ships.entity_interaction.IDraggable;
 
 @Mixin(value = Entity.class, priority = 1)
@@ -26,41 +26,58 @@ public abstract class MixinEntityIntrinsic {
     public double posZ;
     @Shadow
     public World world;
-    public Entity thisClassAsAnEntity = Entity.class.cast(this);
+    @Shadow
+    public boolean collided;
+
+    private final Entity thisClassAsAnEntity = Entity.class.cast(this);
+    private final IDraggable thisClassAsDraggable = IDraggable.class.cast(this);
+    // Used to remember alteredMovement, so that it can be passed from changeMoveArgs() to postMove()
     private IntermediateMovementVariableStorage alteredMovement = null;
-    private boolean hasChanged = false;
+    // We only want to run onEntityPreMove() code when Minecraft calls Entity.move(), not when we call it ourselves.
+    // We use this boolean to keep track of when we've called move() ourselves.
+    private boolean didMinecraftInvokeMove = true;
 
     @Shadow
     public abstract void move(MoverType type, double x, double y, double z);
 
-    @Shadow public boolean collided;
-
+    /**
+     * The goal of this injection is to adjust the arguments passed into move(). By adjusting the arguments we can add
+     * collision between entities and ships.
+     */
     @Inject(method = "move", at = @At("HEAD"), cancellable = true)
-    private void changeMoveArgs(MoverType type, double dx, double dy, double dz,
+    private void onEntityPreMove(MoverType type, double dx, double dy, double dz,
         CallbackInfo callbackInfo) {
-        if (!hasChanged) {
+        // Only run this code if Minecraft invoked move().
+        if (didMinecraftInvokeMove) {
             alteredMovement = EntityMoveInjectionMethods
                 .handleMove(type, dx, dy, dz, thisClassAsAnEntity);
             if (alteredMovement != null) {
-                hasChanged = true;
+                // We're about to invoke move, so set didMinecraftInvokeMove to false.
+                didMinecraftInvokeMove = false;
                 this.move(type, alteredMovement.dxyz.x(), alteredMovement.dxyz.y(),
                     alteredMovement.dxyz.z());
-                hasChanged = false;
+                // Now our invocation of move has finished, so set didMinecraftInvokeMove back to true.
+                didMinecraftInvokeMove = true;
+                // Since we've already called move() in the code above, we must cancel the original invocation of move.
+                // If we didn't cancel the original invocation then move() would be twice, which is obviously wrong.
                 callbackInfo.cancel();
             }
         }
     }
 
+    /**
+     * The goal of this injection is to correctly setup {@link IDraggable#getEntityShipMovementData()} for this Entity.
+     * Specifically this code handles the last ship touched by the entity, as well as how many ticks ago that touch was.
+     */
     @Inject(method = "move", at = @At("RETURN"))
-    private void postMove(CallbackInfo callbackInfo) {
-        final IDraggable entityDraggable = IDraggable.class.cast(this);
-        final EntityShipMovementData oldEntityShipMovementData = entityDraggable.getEntityShipMovementData();
+    private void onEntityPostMove(CallbackInfo callbackInfo) {
+        final EntityShipMovementData oldEntityShipMovementData = thisClassAsDraggable.getEntityShipMovementData();
         if (alteredMovement != null) {
             // If alteredMovement isn't null then we're touching a ship.
             final EntityShipMovementData newEntityShipMovementData = oldEntityShipMovementData
                     .withLastTouchedShip(alteredMovement.shipTouched)
                     .withTicksSinceTouchedShip(0);
-            entityDraggable.setEntityShipMovementData(newEntityShipMovementData);
+            thisClassAsDraggable.setEntityShipMovementData(newEntityShipMovementData);
             EntityCollisionInjector.alterEntityMovementPost(thisClassAsAnEntity, alteredMovement);
         } else {
             if (this.collided) {
@@ -68,12 +85,12 @@ public abstract class MixinEntityIntrinsic {
                 final EntityShipMovementData newEntityShipMovementData = new EntityShipMovementData(
                         null, 0, new Vector3d(), 0
                 );
-                entityDraggable.setEntityShipMovementData(newEntityShipMovementData);
+                thisClassAsDraggable.setEntityShipMovementData(newEntityShipMovementData);
             } else {
                 // If we're not collided and alteredMovement is null, then we're in the air.
                 final EntityShipMovementData newEntityShipMovementData = oldEntityShipMovementData
                         .withTicksSinceTouchedShip(oldEntityShipMovementData.getTicksSinceTouchedShip() + 1);
-                entityDraggable.setEntityShipMovementData(newEntityShipMovementData);
+                thisClassAsDraggable.setEntityShipMovementData(newEntityShipMovementData);
             }
         }
     }

49abdf0f79c8b37ec3a0d31aa3f5ba8c6dd58f4b
Tri0de <best108@uw.edu>
2020-09-18 15:59:25 -0700

    Fixed "moved wrongly" errors, but fall damage is broken

--- src/main/java/org/valkyrienskies/mixin/network/MixinNetHandlerPlayServer.java
+++ src/main/java/org/valkyrienskies/mixin/network/MixinNetHandlerPlayServer.java
@@ -2,7 +2,9 @@ package org.valkyrienskies.mixin.network;
 
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.network.play.server.SPacketPlayerPosLook;
+import net.minecraft.util.Tuple;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
@@ -13,25 +15,41 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.valkyrienskies.mod.common.config.VSConfig;
+import org.valkyrienskies.mod.common.network.IHasPlayerMovementData;
+import org.valkyrienskies.mod.common.network.PlayerMovementData;
+import org.valkyrienskies.mod.common.ships.QueryableShipData;
 import org.valkyrienskies.mod.common.ships.ShipData;
 import org.valkyrienskies.mod.common.ships.chunk_claims.ShipChunkAllocator;
+import org.valkyrienskies.mod.common.ships.entity_interaction.IDraggable;
+import org.valkyrienskies.mod.common.ships.ship_transform.ShipTransform;
+import org.valkyrienskies.mod.common.util.VSMath;
 import org.valkyrienskies.mod.common.util.ValkyrienUtils;
 import valkyrienwarfare.api.TransformType;
 
 import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 
-/**
- * Todo: Replace this with forge events and capabilities.
- */
-@Deprecated
-@Mixin(value = NetHandlerPlayServer.class, priority = 5)
+@Mixin(value = NetHandlerPlayServer.class)
 public abstract class MixinNetHandlerPlayServer {
+
+    private final NetHandlerPlayServer thisAsNetHandler = NetHandlerPlayServer.class.cast(this);
+    private boolean redirectingSetPlayerLocation = false;
 
     @Shadow
     public EntityPlayerMP player;
-    private NetHandlerPlayServer thisAsNetHandler = NetHandlerPlayServer.class.cast(this);
-    private boolean redirectingSetPlayerLocation = false;
+    @Shadow
+    public double firstGoodX;
+    @Shadow
+    public double firstGoodY;
+    @Shadow
+    public double firstGoodZ;
+    @Shadow
+    public double lastGoodX;
+    @Shadow
+    public double lastGoodY;
+    @Shadow
+    public double lastGoodZ;
 
     /**
      * Fixes things such that when mods try to teleport players into the ship space, VS will either
@@ -78,6 +96,77 @@ public abstract class MixinNetHandlerPlayServer {
                 }
                 redirectingSetPlayerLocation = false;
             }
+        }
+    }
+
+    /**
+     * This mixin fixes "Player Moved Wrongly" errors.
+     *
+     * @param packetPlayer The packet the player sent us
+     * @param info We can use this to cancel the invocation
+     */
+    @Inject(method = "processPlayer", at = @At("HEAD"))
+    private void preProcessPlayer(final CPacketPlayer packetPlayer, final CallbackInfo info) {
+        // Don't run any of this code on the network thread!
+        if (this.player.getServerWorld().isCallingFromMinecraftThread()) {
+            final PlayerMovementData addedPlayerMovementData = IHasPlayerMovementData.class.cast(packetPlayer).getPlayerMovementData();
+            final World world = player.world;
+
+            final UUID lastTouchedShipId = addedPlayerMovementData.getLastTouchedShipId();
+            final int ticksSinceTouchedLastShip = addedPlayerMovementData.getTicksSinceTouchedLastShip();
+            final int ticksPartOfGround = addedPlayerMovementData.getTicksPartOfGround();
+            final Vector3d playerPosInShip = new Vector3d(addedPlayerMovementData.getPlayerPosInShip());
+            final Vector3d playerLookInShip = new Vector3d(addedPlayerMovementData.getPlayerLookInShip());
+
+            ShipData lastTouchedShip = null;
+            if (lastTouchedShipId != null) {
+                final QueryableShipData queryableShipData = QueryableShipData.get(world);
+                final Optional<ShipData> shipDataOptional = queryableShipData.getShip(lastTouchedShipId);
+                if (shipDataOptional.isPresent()) {
+                    lastTouchedShip = shipDataOptional.get();
+                    final ShipTransform shipTransform = lastTouchedShip.getShipTransform();
+                    shipTransform.transformPosition(playerPosInShip, TransformType.SUBSPACE_TO_GLOBAL);
+                    shipTransform.transformDirection(playerLookInShip, TransformType.SUBSPACE_TO_GLOBAL);
+                } else {
+                    // Rare case, just ignore this
+                    // info.cancel();
+                    return;
+                }
+            }
+
+            // Get the player pitch/yaw from the look vector
+            final Tuple<Double, Double> pitchYawTuple = VSMath.getPitchYawFromVector(playerLookInShip);
+            final double playerPitchInGlobal = pitchYawTuple.getFirst();
+            final double playerYawInGlobal = pitchYawTuple.getSecond();
+
+            // This should fix the player not having fall damage, but it doesn't.
+            final double originalPlayerY = this.player.posY;
+            this.player.handleFalling(originalPlayerY - playerPosInShip.y(), packetPlayer.isOnGround());
+            // Update the player position values.
+            this.player.setPosition(playerPosInShip.x(), playerPosInShip.y(), playerPosInShip.z());
+
+            // Then update the packet values to match the ones above.
+            packetPlayer.x = playerPosInShip.x();
+            packetPlayer.y = playerPosInShip.y();
+            packetPlayer.z = playerPosInShip.z();
+            packetPlayer.yaw = (float) playerYawInGlobal;
+            packetPlayer.pitch = (float) playerPitchInGlobal;
+
+            // Just set the values to be whats in the packet
+            this.firstGoodX = this.lastGoodX = packetPlayer.x;
+            this.firstGoodY = this.lastGoodY = packetPlayer.y;
+            this.firstGoodZ = this.lastGoodZ = packetPlayer.z;
+
+            // Update the player draggable
+            final IDraggable playerAsDraggable = IDraggable.class.cast(this.player);
+            playerAsDraggable.setEntityShipMovementData(
+                    playerAsDraggable.getEntityShipMovementData()
+                            .withLastTouchedShip(lastTouchedShip)
+                            .withAddedLinearVelocity(new Vector3d())
+                            .withAddedYawVelocity(0)
+                            .withTicksPartOfGround(ticksPartOfGround)
+                            .withTicksSinceTouchedShip(ticksSinceTouchedLastShip)
+            );
         }
     }
 
--- src/main/java/org/valkyrienskies/mixin/network/play/client/MixinCPacketPlayer.java
+++ src/main/java/org/valkyrienskies/mixin/network/play/client/MixinCPacketPlayer.java
@@ -10,6 +10,7 @@ import net.minecraft.world.World;
 import org.joml.Vector3d;
 import org.joml.Vector3dc;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -28,58 +29,19 @@ import java.util.UUID;
 @Mixin(CPacketPlayer.class)
 public class MixinCPacketPlayer implements IHasPlayerMovementData {
 
+    @Shadow
+    public double x;
+    @Shadow
+    public double y;
+    @Shadow
+    public double z;
+    @Shadow
+    public float yaw;
+    @Shadow
+    public float pitch;
+
     // Piggyback this with every CPacketPlayer.
     private PlayerMovementData addedPlayerMovementData;
-
-    @Inject(method = "processPacket", at = @At(value = "HEAD"), cancellable = true)
-    private void preProcessPacket(final INetHandlerPlayServer handler, final CallbackInfo info) {
-        final EntityPlayer player = ((NetHandlerPlayServer) handler).player;
-        final World world = player.world;
-
-        final UUID lastTouchedShipId = addedPlayerMovementData.getLastTouchedShipId();
-        final int ticksSinceTouchedLastShip = addedPlayerMovementData.getTicksSinceTouchedLastShip();
-        final int ticksPartOfGround = addedPlayerMovementData.getTicksPartOfGround();
-        final Vector3d playerPosInShip = new Vector3d(addedPlayerMovementData.getPlayerPosInShip());
-        final Vector3d playerLookInShip = new Vector3d(addedPlayerMovementData.getPlayerLookInShip());
-
-        ShipData lastTouchedShip = null;
-        if (lastTouchedShipId != null) {
-            final QueryableShipData queryableShipData = QueryableShipData.get(world);
-            final Optional<ShipData> shipDataOptional = queryableShipData.getShip(lastTouchedShipId);
-            if (shipDataOptional.isPresent()) {
-                lastTouchedShip = shipDataOptional.get();
-                final ShipTransform shipTransform = lastTouchedShip.getShipTransform();
-                shipTransform.transformPosition(playerPosInShip, TransformType.SUBSPACE_TO_GLOBAL);
-                shipTransform.transformDirection(playerLookInShip, TransformType.SUBSPACE_TO_GLOBAL);
-            } else {
-                // Rare case, just ignore this
-                return;
-            }
-        }
-
-        // Get the player pitch/yaw from the look vector
-        final Tuple<Double, Double> pitchYawTuple = VSMath.getPitchYawFromVector(playerLookInShip);
-        final double playerPitchInGlobal = pitchYawTuple.getFirst();
-        final double playerYawInGlobal = pitchYawTuple.getSecond();
-
-        // Maybe just set the position directly?
-        player.setPositionAndRotation(playerPosInShip.x(), playerPosInShip.y(), playerPosInShip.z(),
-                (float) playerYawInGlobal, (float) playerPitchInGlobal);
-        player.setRotationYawHead((float) playerYawInGlobal);
-        player.onGround = addedPlayerMovementData.isOnGround();
-
-        final IDraggable playerAsDraggable = IDraggable.class.cast(player);
-        playerAsDraggable.setEntityShipMovementData(
-                playerAsDraggable.getEntityShipMovementData()
-                        .withLastTouchedShip(lastTouchedShip)
-                        .withAddedLinearVelocity(new Vector3d())
-                        .withAddedYawVelocity(0)
-                        .withTicksPartOfGround(ticksPartOfGround)
-                        .withTicksSinceTouchedShip(ticksSinceTouchedLastShip)
-        );
-
-        info.cancel();
-    }
 
     @Inject(method = "readPacketData", at = @At(value = "HEAD"))
     private void preReadPacketData(final PacketBuffer packetBuffer, final CallbackInfo info) {

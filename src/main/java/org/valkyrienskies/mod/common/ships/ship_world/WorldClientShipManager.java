b3a3f1d93031d5a00c2643ffa453761065740c64
Rubydesic <rubydesic@gmail.com>
2020-10-11 20:17:50 -0400

    Replace IllegalStateException throws with an error + ignore

--- src/main/java/org/valkyrienskies/mod/common/ships/ship_world/WorldClientShipManager.java
+++ src/main/java/org/valkyrienskies/mod/common/ships/ship_world/WorldClientShipManager.java
@@ -4,6 +4,8 @@ import com.google.common.collect.ImmutableList;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.valkyrienskies.mod.common.config.VSConfig;
 import org.valkyrienskies.mod.common.ships.QueryableShipData;
 import org.valkyrienskies.mod.common.ships.ShipData;
@@ -20,6 +22,7 @@ public class WorldClientShipManager implements IPhysObjectWorld {
     // Use LinkedHashSet as a queue because it preserves order and doesn't allow duplicates
     private final LinkedHashSet<UUID> loadQueue, unloadQueue;
     private ImmutableList<PhysicsObject> threadSafeLoadedShips;
+    private static final Logger logger = LogManager.getLogger();
 
     public WorldClientShipManager(World world) {
         this.world = world;
@@ -52,11 +55,13 @@ public class WorldClientShipManager implements IPhysObjectWorld {
         // Load ships queued for loading
         for (final UUID toLoadID : loadQueue) {
             if (loadedShips.containsKey(toLoadID)) {
-                throw new IllegalStateException("Tried loading a for ship that was already loaded? UUID is\n" + toLoadID);
+                logger.error("Tried loading a for ship that was already loaded? UUID is\n" + toLoadID);
+                continue;
             }
             Optional<ShipData> toLoadOptional = queryableShipData.getShip(toLoadID);
             if (!toLoadOptional.isPresent()) {
-                throw new IllegalStateException("No ship found for UUID:\n" + toLoadID);
+                logger.error("No ship found for UUID:\n" + toLoadID);
+                continue;
             }
             ShipData shipData = toLoadOptional.get();
             PhysicsObject physicsObject = new PhysicsObject(world, shipData);
@@ -70,7 +75,8 @@ public class WorldClientShipManager implements IPhysObjectWorld {
         // Unload ships queued for unloading
         for (final UUID toUnloadID : unloadQueue) {
             if (!loadedShips.containsKey(toUnloadID)) {
-                throw new IllegalStateException("Tried unloading that isn't loaded? ID is\n" + toUnloadID);
+                logger.error("Tried unloading that isn't loaded? ID is\n" + toUnloadID);
+                continue;
             }
             PhysicsObject removedShip = loadedShips.get(toUnloadID);
             removedShip.unload();
